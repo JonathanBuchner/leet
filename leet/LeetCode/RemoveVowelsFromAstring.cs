395f5b8abe9136e7f56a4839a4c8e8cea82b66a4
Jonathan Buchner <v-jobuch@microsoft.com>
2021-06-20 09:24:32 -0700

    Add problem REmove Vowels From A String

---
+++ leet/LeetCode/RemoveVowelsFromAstring.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.RemoveVowelsFromAstring
+{
+    public class Solution
+    {
+        public string RemoveVowels(string s)
+        {
+
+            var sb = new System.Text.StringBuilder(s);
+            var i = 0;
+
+            while (i < sb.Length)
+            {
+                if
+                (
+                    sb[i] == 'a' ||
+                    sb[i] == 'e' ||
+                    sb[i] == 'i' ||
+                    sb[i] == 'o' ||
+                    sb[i] == 'u'
+                )
+                {
+                    sb.Remove(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    // Faster solution
+    /*
+        public class Solution {
+            public string RemoveVowels(string s) {
+                char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < s.Length; i++) {
+                    if(Array.IndexOf(vowels, s[i]) < 0) {
+                        sb.Append(s[i]);
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+    */
+}

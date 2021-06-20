b9a25b9bfeca7cc7a052c61232ad1ad9146f84ad
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-20 11:23:20 -0700

    Add LeetCode problem Longest Substring Without Repeating Characters

---
+++ leet/LeetCode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * 
+ * Given a string s, find the length of the longest substring without repeating characters.
+ */
+namespace LeetCode.LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+
+            var longest = 0;
+            var front = 0;
+            var back = 0;
+            var seen = new HashSet<char>() { };
+
+            while (front < s.Length)
+            {
+                if (seen.Contains(s[front]))
+                {
+                    seen.Remove(s[back]);
+                    back++;
+                }
+                else
+                {
+                    seen.Add(s[front]);
+                    if (longest < seen.Count) longest = seen.Count;
+                    front++;
+                }
+
+            }
+
+            return longest;
+        }
+    }
+}
---
+++ leet_test/LeetCode/LongestSubstringWithoutRepeatingCharacters_tests.cs
@@ -0,0 +1,54 @@
+using System;
+using LeetCode.LongestSubstringWithoutRepeatingCharacters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode
+{
+    [TestClass]
+    public class LongestSubstringWithoutRepeatingCharacters_tests
+    {
+        [TestMethod]
+        public void Example1_can_calculate_length()
+        {
+            var test = "abcabcbb";
+            var sut = new Solution();
+
+            var response = sut.LengthOfLongestSubstring(test);
+
+            Assert.AreEqual(3, response);
+        }
+
+        [TestMethod]
+        public void Example2_return_one_for_repeating_string()
+        {
+            var test = "bbbbb";
+            var sut = new Solution();
+
+            var response = sut.LengthOfLongestSubstring(test);
+
+            Assert.AreEqual(1, response);
+        }
+
+        [TestMethod]
+        public void Example3_calculates_substring_not_sequence()
+        {
+            var test = "pwwkew";
+            var sut = new Solution();
+
+            var response = sut.LengthOfLongestSubstring(test);
+
+            Assert.AreEqual(3, response);
+        }
+
+        [TestMethod]
+        public void Example1_empty_lists_returns_0()
+        {
+            var test = "";
+            var sut = new Solution();
+
+            var response = sut.LengthOfLongestSubstring(test);
+
+            Assert.AreEqual(0, response);
+        }
+    }
+}

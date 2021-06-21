3e6b47c40f86d93b051590cdb4303f36c22de4bd
Jonathan Buchner <v-jobuch@microsoft.com>
2021-06-21 08:25:37 -0700

    Add tests for ValidPalindrome.

---
+++ leet/LeetCode/ValidPalindrome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.ValidPalindrome
+{
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            return false;
+        }
+    }
+}
---
+++ leet_test/LeetCode/ValidPalindrome_tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.ValidPalindrome;
+
+namespace leet_test.LeetCode
+{
+    [TestClass]
+    public class ValidPalindrome_tests
+    {
+        [TestMethod]
+        public void Example1_Can_indentify_palindrom()
+        {
+            var input = "A man, a plan, a canal: Panama";
+            var expected = true;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Example1_Can_indentify_not_a_palindrom()
+        {
+            var input = "race a car";
+            var expected = false;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

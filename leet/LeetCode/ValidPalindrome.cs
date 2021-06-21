3af2009ccaf547c7464482caece788dac27db9a0
Jonathan Buchner <v-jobuch@microsoft.com>
2021-06-21 08:53:42 -0700

    My first submission.

--- leet/LeetCode/ValidPalindrome.cs
+++ leet/LeetCode/ValidPalindrome.cs
@@ -4,11 +4,39 @@ using System.Text;
 
 namespace leet.LeetCode.ValidPalindrome
 {
+    /* Valid Palindrome
+     * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/162/
+     * 
+     * Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+     */
+
     public class Solution
     {
         public bool IsPalindrome(string s)
         {
-            return false;
+            if (s.Length < 2) return false;
+
+            //Prepare string for onsidering only alphanumeric characters and ignoring cases.
+            var arr = s.ToLower().ToCharArray();
+            var sb = new StringBuilder();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (Char.IsLetter(arr[i])) sb.Append(arr[i]);
+            }
+            var str = sb.ToString();
+
+            var left = 0;
+            var right = str.Length - 1;
+
+            while (right > left)
+            {
+                if (str[right] != str[left]) return false;
+
+                left++;
+                right--;
+            }
+
+            return true;
         }
     }
 }
--- leet_test/LeetCode/ValidPalindrome_tests.cs
+++ leet_test/LeetCode/ValidPalindrome_tests.cs
@@ -22,9 +22,69 @@ namespace leet_test.LeetCode
         }
 
         [TestMethod]
-        public void Example1_Can_indentify_not_a_palindrom()
+        public void Example2_Can_indentify_not_a_palindrom()
         {
             var input = "race a car";
+            var expected = false;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Can_indentify_odd_number_palindrom()
+        {
+            var input = "adcda";
+            var expected = true;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Can_indentify_even_number_palindrom()
+        {
+            var input = "adccda";
+            var expected = true;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Palindrom_can_be_two_length()
+        {
+            var input = "aa";
+            var expected = true;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Palindrom_cant_be_one_length()
+        {
+            var input = "a";
+            var expected = false;
+            var sut = new Solution();
+
+            var result = sut.IsPalindrome(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Palindrom_cant_be_zero_length()
+        {
+            var input = "";
             var expected = false;
             var sut = new Solution();
 

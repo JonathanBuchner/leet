5732839e02a0d18a3bcaff9ec850dfc6226934f9
Jonathan Buchner <v-jobuch@microsoft.com>
2021-06-22 15:55:00 -0700

    Prettify

--- leet/LeetCode/TwoSum.cs
+++ leet/LeetCode/TwoSum.cs
@@ -5,18 +5,17 @@ using System.Text;
 
 namespace leet.LeetCode.TwoSum
 {
+    /* Two Sum
+    * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    * You can return the answer in any order.
+    * 
+    * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/173/
+    */
     public class Solution
     {
-        /* Two Sum
-         * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-         * You may assume that each input would have exactly one solution, and you may not use the same element twice.
-         * You can return the answer in any order.
-         * 
-         * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/173/
-         */
         public int[] TwoSum(int[] nums, int target)
         {
-
             var dictionary = new Dictionary<int, int>();
             
             for (var i = 0; i < nums.Length; i++)
@@ -31,9 +30,7 @@ namespace leet.LeetCode.TwoSum
                 {
                     dictionary.Add(nums[i], i);
                 }
-
             }
-
 
             throw new Exception("No answer");
         }

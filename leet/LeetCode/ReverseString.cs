2f4baa02b048b741e97b9997fbf573263ec6f874
v-jobuch <me@jonathanbuchner.com>
2021-06-23 07:51:08 -0700

    Set up tests

---
+++ leet/LeetCode/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/* Reverse String
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+ * 
+ * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/187/
+ */
+namespace leet.LeetCode.ReverseString
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+
+        }
+    }
+}
---
+++ leet_test/LeetCode/ReverseString_tests.cs
@@ -0,0 +1,33 @@
+﻿using leet.LeetCode.ReverseString;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode
+{
+    [TestClass]
+    public class ReverseString_tests
+    {
+        [TestMethod]
+        public void Example1_can_reverse_string()
+        {
+            var input = new char[] { 'h', 'e', 'l', 'l', 'o' };
+            var expected = new char[] { 'o', 'l', 'l', 'e', 'h'};
+            var sut = new Solution();
+
+            sut.ReverseString(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void Example2_can_reverse_string_with_capitals()
+        {
+            var input = new char[] { 'H', 'a', 'n', 'n', 'a', 'h'};
+            var expected = new char[] { 'h', 'a', 'n', 'n', 'a', 'H' };
+            var sut = new Solution();
+
+            sut.ReverseString(input);
+
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}

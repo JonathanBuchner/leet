766ef080350af77a6bf78a93ae10cf6760e0aaed
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-27 08:37:38 -0700

    Add tests for trapping rainwater

---
+++ leet/LeetCode/TrappingRainWater.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace leet.LeetCode.TrappingRainWater
+{
+    /*
+     * Trapping Rain Water
+     * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+     * 
+     * Constraints:
+     *  - n == height.length
+     *  - 0 <= n <= 3 * 104
+     *  - 0 <= height[i] <= 105
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/211/
+     */
+
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/TrappingRainwaterTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.TrappingRainWater;
+
+namespace leet_test.LeetCode
+{
+    [TestClass]
+    public class TrappingRainwaterTests
+    {
+        [TestMethod]
+        public void Example1_can_compute_water_trapped()
+        {
+            var input = new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+            var expected = 6;
+            var sut = new Solution();
+
+            var actual = sut.Trap(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example1_can_compute_water_trapped_when_highest_numbers_on_the_side()
+        {
+            var input = new int[] { 4, 2, 0, 3, 2, 5 };
+            var expected = 9;
+            var sut = new Solution();
+
+            var actual = sut.Trap(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

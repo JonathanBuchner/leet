4f5026d84d014b532ddd0a2ae132c4d6cd068952
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-28 13:55:25 -0700

    Add tests and set up for inserting items int an array

---
+++ leet/LeetCode/Modules/Arrays/Inserting/MergeSortedArrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.Inserting.MergeSortedArrays
+{
+    /*
+     * Merge two sorted arrays
+     * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+     * 
+     * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n
+     * 
+     * https://leetcode.com/explore/featured/card/fun-with-arrays/525/inserting-items-into-an-array/3253/
+     */
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/Inserting/MergeSortedArrays_tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Modules.Arrays.Inserting.MergeSortedArrays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Modules.Arrays.Inserting
+{
+    [TestClass]
+    public class MergeSortedArrays_tests
+    {
+        [TestMethod]
+        public void Example1_Can_merge_two_arrays()
+        {
+            var input1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            var input1Length = 3;
+            var input2 = new int[] { 2, 5, 6 };
+            var input2Length = 3;
+            var expected = new int[] { 1, 2, 2, 3, 5, 6 };
+            var sut = new Solution();
+
+            sut.Merge(input1, input1Length, input2, input2Length);
+
+            CollectionAssert.AreEqual(expected, input1);
+        }
+
+        [TestMethod]
+        public void Example2_Can_merge_two_arrays_if_the_second_array_is_empty()
+        {
+            var input1 = new int[] { 1 };
+            var input1Length = 1;
+            var input2 = new int[] { };
+            var input2Length = 0;
+            var expected = new int[] { 1 };
+            var sut = new Solution();
+
+            sut.Merge(input1, input1Length, input2, input2Length);
+
+            CollectionAssert.AreEqual(expected, input1);
+        }
+
+        [TestMethod]
+        public void Example3_Can_merge_two_arrays_if_the_first_array_is_empty()
+        {
+            var input1 = new int[] { 0 };
+            var input1Length = 0;
+            var input2 = new int[] { 1 };
+            var input2Length = 1;
+            var expected = new int[] { 1 };
+            var sut = new Solution();
+
+            sut.Merge(input1, input1Length, input2, input2Length);
+
+            CollectionAssert.AreEqual(expected, input1);
+        }
+    }
+}

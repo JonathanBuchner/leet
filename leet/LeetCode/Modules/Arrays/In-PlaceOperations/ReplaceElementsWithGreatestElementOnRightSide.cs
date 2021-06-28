423254293e3f84c50228d92991e670e767616b5d
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-28 07:45:32 -0700

    Add tests and change folder structure for Replace Elements With...

---
+++ leet/LeetCode/Modules/Arrays/In-PlaceOperations/ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.In_PlaceOperations.ReplaceElementsWithGreatestElementOnRightSide
+{
+    /*
+     * Replace Elements with Greatest Element on Right Side
+     * Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+     * 
+     * https://leetcode.com/explore/featured/card/fun-with-arrays/511/in-place-operations/3259/
+     */
+
+    public class Solution
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/In-PlaceOperations/ReplaceElementsWithGreatestElementOnRightSide_tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.Arrays.In_PlaceOperations.ReplaceElementsWithGreatestElementOnRightSide;
+
+namespace leet_test.LeetCode.Modules.Arrays.In_PlaceOperations
+{
+    [TestClass]
+    public class ReplaceElementsWithGreatestElementOnRightSide_tests
+    {
+        [TestMethod]
+        public void Example1_Can_Corrrectly_Replace_Elements()
+        {
+            var input = new int[] { 17, 18, 5, 4, 6, 1 };
+            var expected = new int[] { 18, 6, 6, 6, 1, -1 };
+            var sut = new Solution();
+
+            var actual = sut.ReplaceElements(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2_Can_corrrectly_handel_single_entry()
+        {
+            var input = new int[] { 400 };
+            var expected = new int[] { -1 };
+            var sut = new Solution();
+
+            var actual = sut.ReplaceElements(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}

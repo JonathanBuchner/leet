f99d03dab6bb36a1ab215b1ae2843a3803b112bf
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-29 07:43:32 -0700

    Fix zero length arrays & polish solution for removing duplicates from sorted arrays

--- leet/LeetCode/Modules/Arrays/Deleting/RemoveDuplicatesFromSortedArray.cs
+++ leet/LeetCode/Modules/Arrays/Deleting/RemoveDuplicatesFromSortedArray.cs
@@ -4,10 +4,18 @@ using System.Text;
 
 namespace leet.LeetCode.Modules.Arrays.Deleting.RemoveDuplicatesFromSortedArray
 {
+    /*
+     * Remove Duplicates from Sorted Array
+     * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+     *  
+     * https://leetcode.com/explore/featured/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+     */
     public class Solution
     {
         public int RemoveDuplicates(int[] nums)
         {
+            if (nums.Length < 1) return 0;
+
             var ind = 0;
             var pos = 1;
 

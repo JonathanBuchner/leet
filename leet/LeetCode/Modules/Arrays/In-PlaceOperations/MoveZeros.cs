58c4bf5131c070af6c1b609e0433fdd4c1271cb1
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-29 08:57:16 -0700

    Simplify solution for move zereos

--- leet/LeetCode/Modules/Arrays/In-PlaceOperations/MoveZeros.cs
+++ leet/LeetCode/Modules/Arrays/In-PlaceOperations/MoveZeros.cs
@@ -19,23 +19,23 @@ namespace leet.LeetCode.Modules.Arrays.In_PlaceOperations.MoveZeros
             if (nums.Length < 2) return;
 
             var back = 0;
-            while (back < nums.Length && nums[back] != 0) back++;
-
-            if (back == nums.Length - 1) return;
+            var front = 0;
                 
-            var front = back + 1;
             while (front < nums.Length)
             {
                 if (nums[front] != 0)
                 {
-                    var temp = nums[back];
                     nums[back] = nums[front];
-                    nums[front] = temp;
-
                     back++;
                 }
 
                 front++;
+            }
+
+            while (back < nums.Length)
+            {
+                nums[back] = 0;
+                back++;
             }
         }
     }

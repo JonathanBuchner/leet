8e7ff18fc0ca560ef07adb8b074df71b5d31b9c1
Jonathan Buchner <me@jonathanbuchner.com>
2021-06-29 07:12:04 -0700

    Add tests for Removing duplicates from sorted array

---
+++ leet/LeetCode/Modules/Arrays/Deleting/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.Deleting.RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/Delete/RemoveDuplicatesFromSortedArray_tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using leet.LeetCode.Modules.Arrays.Deleting.RemoveDuplicatesFromSortedArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Modules.Arrays.Delete
+{
+    [TestClass]
+    public class RemoveDuplicatesFromSortedArray_tests
+    {
+        [TestMethod]
+        public void Example1_can_return_correct_length_and_delete_from_array()
+        {
+            var input = new int[] { 1, 1, 2 };
+            var expected = 2;
+            var expectedArray = new int[] { 1, 2 };
+            var sut = new Solution();
+
+            var actual = sut.RemoveDuplicates(input);
+
+            Assert.AreEqual(expected, actual);
+
+            for(var i = 0; i < expectedArray.Length; i++)
+            {
+                Assert.AreEqual(expectedArray[i], input[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Example2_can_return_correct_length_and_delete_from_array()
+        {
+            var input = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var expected = 5;
+            var expectedArray = new int[] { 0, 1, 2, 3, 4 };
+            var sut = new Solution();
+
+            var actual = sut.RemoveDuplicates(input);
+
+            Assert.AreEqual(expected, actual);
+
+            for (var i = 0; i < expectedArray.Length; i++)
+            {
+                Assert.AreEqual(expectedArray[i], input[i]);
+            }
+        }
+    }
+}

58c4810f1664873840bba445e32397ee80d95686
Jonathan Buchner <jbuchner@ext.aerotek.com>
2021-06-30 19:17:52 -0700

    Add tests for sort array by parity.

---
+++ leet/LeetCode/Modules/Arrays/In-PlaceOperations/SortArraysByParity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.In_PlaceOperations.SortArraysByParity
+{
+    /*
+     * Sort Array By Parity
+
+     * Given an array nums of non-negative integers, return an array consisting of all the even elements of nums, followed by all the odd elements of nums.
+     * 
+     * You may return any answer array that satisfies this condition.
+     * 
+     * https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3260/
+     */
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/In-PlaceOperations/SortArrayByParity_tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.Arrays.In_PlaceOperations.SortArraysByParity;
+
+namespace leet_test.LeetCode.Modules.Arrays.In_PlaceOperations
+{
+    [TestClass]
+    public class SortArrayByParity_tests
+    {
+        [TestMethod]
+        public void Examplel1_Can_sort_by_even_odd_parity()
+        {
+            var input = new int[] { 3, 1, 2, 4 };
+            var expected = new int[] { 2, 4, 3, 1 };
+            var sut = new Solution();
+
+            var actual = sut.SortArrayByParity(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}

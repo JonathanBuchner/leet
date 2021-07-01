9f5dcf4fa62cc143af38811f895c46cf102ea007
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-01 06:24:58 -0700

    Add tests for height checker

---
+++ leet/LeetCode/Modules/Arrays/Conclusion/HeightChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.Conclusion.HeightChecker
+{
+    /*
+     * Height Checker
+     * 
+     * A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+     * 
+     * You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+     * 
+     * Return the number of indices where heights[i] != expected[i].
+     * 
+     * https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+     */
+    public class Solution
+    {
+        public int HeightChecker(int[] heights)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/Conclusion/HeightsChecker_tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Modules.Arrays.Conclusion.HeightChecker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Modules.Arrays.Conclusion
+{
+    [TestClass]
+    public class HeightsChecker_tests
+    {
+        [TestMethod]
+        public void Example1_Can_return_indecies_that_are_out_of_line()
+        {
+            var input = new int[] { 1, 1, 4, 2, 1, 3 };
+            var expected = 3;
+            var sut = new Solution();
+
+            var actual = sut.HeightChecker(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2_Can_detect_if_every_number_is_in_incorrect_position()
+        {
+            var input = new int[] { 5, 1, 2, 3, 4 };
+            var expected = 5;
+            var sut = new Solution();
+
+            var actual = sut.HeightChecker(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3_Can_dectect_if_every_number_is_in_the_correct_position()
+        {
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            var expected = 0;
+            var sut = new Solution();
+
+            var actual = sut.HeightChecker(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

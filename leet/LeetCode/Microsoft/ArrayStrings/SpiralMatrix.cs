9b2f9930f37bd33033e7a9dd6b9acf78a22249ab
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-05 08:00:41 -0700

    First submitted solution for spiral matrix

--- leet/LeetCode/Microsoft/ArrayStrings/SpiralMatrix.cs
+++ leet/LeetCode/Microsoft/ArrayStrings/SpiralMatrix.cs
@@ -15,7 +15,43 @@ namespace leet.LeetCode.Microsoft.ArrayStrings.SpiralMatrix
     {
         public IList<int> SpiralOrder(int[][] matrix)
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            var o = 0;
+            var row = matrix[0].Length - 1;
+            var col = matrix.Length - 1;
+
+            while (list.Count < matrix.Length * matrix[0].Length)
+            {
+                for (var r = o; r < matrix[o].Length - o; r++)
+                {
+                    list.Add(matrix[o][r]);
+                }
+
+                for (var c = 1 + o; c < matrix.Length - o; c++)
+                {
+                    list.Add(matrix[c][row - o]);
+                }
+
+                if (2 * o  + 1 < matrix.Length)
+                {
+                    for (var r = row - 1 - o; r >= o; r--)
+                    {
+                        list.Add(matrix[col - o][r]);
+                    }
+
+                    if (2 * o + 1 < matrix[0].Length)
+                    {
+                        for (var c = col - 1 - o; c > o; c--)
+                        {
+                            list.Add(matrix[c][o]);
+                        }
+                    }
+                }
+
+                o++;
+            }
+
+            return list;
         }
     }
 }

b7ed1e0bd24d745eba2e01a03398591cf09376ec
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-05 06:12:24 -0700

    Add tests for spiral matrix

---
+++ leet/LeetCode/Microsoft/ArrayStrings/SpiralMatrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.ArrayStrings.SpiralMatrix
+{
+    /*
+     * SpiralMatrix
+     * 
+     * Given an m x n matrix, return all elements of the matrix in spiral order.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/178/
+     */
+    public class Solution
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/ArraysStrings/SpiralMatrix_tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Microsoft.ArrayStrings.SpiralMatrix;
+
+namespace leet_test.LeetCode.Microsoft.ArraysStrings
+{
+    [TestClass]
+    public class SpiralMatrix_tests
+    {
+        [TestMethod]
+        public void Example1_Can_return_three_by_three_matrix()
+        {
+            // [1,2,3],[4,5,6],[7,8,9]
+            var input = new int[][]
+            {
+                new int[] { 1, 2 , 3 },
+                new int[] { 4, 5 , 6 },
+                new int[] { 7, 8 , 9 }
+            };
+            IList<int> expected = new List<int> { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+            var sut = new Solution();
+
+            var actual = sut.SpiralOrder(input);
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void Example2_Can_return_three_by_four_matrix()
+        {
+            // [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+            var input = new int[][]
+            {
+                new int[] { 1, 2 , 3, 4 },
+                new int[] { 5, 6 , 7, 8 },
+                new int[] { 9, 10, 11, 12 }
+            };
+            IList<int> expected = new List<int> { 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7 };
+            var sut = new Solution();
+
+            var actual = sut.SpiralOrder(input);
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}

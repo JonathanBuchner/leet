114236df02d6401614853669f018e8ad9bb7bef9
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-06 18:30:48 -0700

    Add tests for Intersetion of Two Linked Lists

---
+++ leet/LeetCode/Modules/LinkedList/IntersectOfTwoLinkedLists.cs
@@ -0,0 +1,22 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.LinkedList.IntersectOfTwoLinkedLists
+{
+    /*
+     * Intersection of Two Linked Lists
+     * 
+     * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+     * 
+     * https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
+     */
+    public class Solution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/LinkedList/IntersectionOfTwoLinkedLists_tests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using leet.LeetCode.Models;
+using leet.LeetCode.Modules.LinkedList;
+using leet.LeetCode.Modules.LinkedList.IntersectOfTwoLinkedLists;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Modules.LinkedList
+{
+    [TestClass]
+    public class IntersectionOfTwoLinkedLists_tests
+    {
+        [TestMethod]
+        public void Example1_Can_detect_intersection_node()
+        {
+            //Combined
+            var combined = new MySinglyLinkedList(new ListNode(8));
+            combined.AddAtTail(4);
+            combined.AddAtTail(5);
+            //List One
+            var alist = new MySinglyLinkedList(new ListNode(4));
+            alist.AddAtTail(1);
+            alist.Tail.next = combined.Head;
+            //List two
+            var blist = new MySinglyLinkedList(new ListNode(5));
+            blist.AddAtTail(6);
+            blist.AddAtTail(1);
+            blist.Tail.next = combined.Head;
+            // Additional set up
+            var expected = combined.Head;
+            var sut = new Solution();
+
+            var actual = sut.GetIntersectionNode(alist.Head, blist.Head);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2_Can_detect_intersection_node_with_a_early_combine()
+        {
+            //Combined
+            var combined = new MySinglyLinkedList(new ListNode(2));
+            combined.AddAtTail(4);
+            //List One
+            var alist = new MySinglyLinkedList(new ListNode(3));
+            alist.Tail.next = combined.Head;
+            //List two
+            var blist = new MySinglyLinkedList(new ListNode(1));
+            blist.AddAtTail(9);
+            blist.AddAtTail(1);
+            blist.Tail.next = combined.Head;
+            // Additional set up
+            var expected = combined.Head;
+            var sut = new Solution();
+
+            var actual = sut.GetIntersectionNode(alist.Head, blist.Head);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3_return_null_if_lists_dont_combine()
+        {
+            var alist = new MySinglyLinkedList(new ListNode(3));
+            //List two
+            var blist = new MySinglyLinkedList(new ListNode(1));
+            blist.AddAtTail(9);
+            blist.AddAtTail(1);
+            // Additional set up
+            ListNode expected = null;
+            var sut = new Solution();
+
+            var actual = sut.GetIntersectionNode(alist.Head, blist.Head);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

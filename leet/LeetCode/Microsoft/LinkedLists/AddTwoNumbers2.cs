a152404760cd566da5ee57962c4b20aeee39c534
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-07 16:57:39 -0700

    Add tests for add two numbers 2

---
+++ leet/LeetCode/Microsoft/LinkedLists/AddTwoNumbers2.cs
@@ -0,0 +1,22 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.LinkedLists.AddTwoNumbers2
+{
+    /*
+     * Add Two Numbers II
+     * 
+     * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/32/linked-list/205/
+     */
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/LinkedLists/AddTwoNumbers2_tests.cs
@@ -0,0 +1,52 @@
+﻿using leet.LeetCode.Microsoft.LinkedLists.AddTwoNumbers2;
+using leet_test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Microsoft.LinkedLists
+{
+    [TestClass]
+    public class AddTwoNumbers2_tests
+    {
+        [TestMethod]
+        public void Example1_Can_add_two_numbers_of_different_length()
+        {
+            var helper = new LinkedListHelper();
+            var input1 = helper.CreateSinglyLinkedList(new int[] { 7, 2, 4, 3 });
+            var input2 = helper.CreateSinglyLinkedList(new int[] { 5, 6, 4 });
+            var expected = helper.CreateSinglyLinkedList(new int[] { 7, 8, 0, 7 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input1, input2);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2_Can_add_two_numbers_of_same_length()
+        {
+            var helper = new LinkedListHelper();
+            var input1 = helper.CreateSinglyLinkedList(new int[] { 2, 4, 3 });
+            var input2 = helper.CreateSinglyLinkedList(new int[] { 5, 6, 4 });
+            var expected = helper.CreateSinglyLinkedList(new int[] { 8, 0, 7 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input1, input2);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3_Can_add_two_zeroes()
+        {
+            var helper = new LinkedListHelper();
+            var input1 = helper.CreateSinglyLinkedList(new int[] { 0 });
+            var input2 = helper.CreateSinglyLinkedList(new int[] { 0 });
+            var expected = helper.CreateSinglyLinkedList(new int[] { 0});
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input1, input2);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+    }
+}

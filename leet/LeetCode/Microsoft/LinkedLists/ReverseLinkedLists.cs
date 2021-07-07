26b798e157966029d59e0b1c667499ecc926956d
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-07 07:46:08 -0700

    Add tests for reverse linked lists

---
+++ leet/LeetCode/Microsoft/LinkedLists/ReverseLinkedLists.cs
@@ -0,0 +1,22 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.LinkedLists.ReverseLinkedLists
+{
+    /*
+     * Reverse Linked Lists
+     * 
+     * Given the head of a singly linked list, reverse the list, and return the reversed list.
+     * 
+     * 
+     */
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/LinkedLists/ReverseLinkedLists_tests.cs
@@ -0,0 +1,50 @@
+﻿using leet.LeetCode.Microsoft.LinkedLists.ReverseLinkedLists;
+using leet_test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Microsoft.LinkedLists
+{
+    [TestClass]
+    public class ReverseLinkedLists_tests
+    {
+        [TestMethod]
+        public void Example1_Can_reverse_list()
+        {
+            var helper = new LinkedListHelper();
+            var list = helper.CreateSinglyLinkedList(new int[] { 1, 2, 3, 4, 5 });
+            var expected = helper.CreateSinglyLinkedList(new int[] {5, 4, 3, 2, 1 });
+            var sut = new Solution();
+
+            var actual = sut.ReverseList(list);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2_Can_reverse_short_list()
+        {
+            var helper = new LinkedListHelper();
+            var list = helper.CreateSinglyLinkedList(new int[] { 1, 2 });
+            var expected = helper.CreateSinglyLinkedList(new int[] { 2, 1 });
+            var sut = new Solution();
+
+            var actual = sut.ReverseList(list);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Example2_Can_handle_list_of_one()
+        {
+            var helper = new LinkedListHelper();
+            var list = helper.CreateSinglyLinkedList(new int[] { 1 });
+            var expected = helper.CreateSinglyLinkedList(new int[] { 1 });
+            var sut = new Solution();
+
+            var actual = sut.ReverseList(list);
+
+            AssertLinkedList.AreEqual(expected, actual);
+        }
+    }
+}

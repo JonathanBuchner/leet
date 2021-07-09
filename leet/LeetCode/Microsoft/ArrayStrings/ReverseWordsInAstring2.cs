6d982c7369f6313b35d59fdaa8072fed7395da5e
Jonathan Buchner <me@jonathanbuchner.com>
2021-07-08 17:32:38 -0700

    Add tests for can reverse words in a string 2 (in place)

---
+++ leet/LeetCode/Microsoft/ArrayStrings/ReverseWordsInAstring2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.ArrayStrings.ReverseWordsInAstring2
+{
+    /*
+     * Reverse Words in a String II
+     * 
+     * Given a character array s, reverse the order of the words.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/30/array-and-strings/213/
+     */
+    public class Solution
+    {
+        public void ReverseWords(char[] s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/ArraysStrings/ReverseWordsInAstring2_tests.cs
@@ -0,0 +1,45 @@
+﻿using leet.LeetCode.Microsoft.ArrayStrings.ReverseWordsInAstring2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Microsoft.ArraysStrings
+{
+    [TestClass]
+    public class ReverseWordsInAstring2_tests
+    {
+        [TestMethod]
+        public void Example1_Can_reverse_a_phrase()
+        {
+            var input = new char[] { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' };
+            var expected = new char[] { 'b', 'l', 'u', 'e', ' ', 'i', 's', ' ', 's', 'k', 'y', ' ', 't', 'h', 'e' };
+            var sut = new Solution();
+
+            sut.ReverseWords(input);
+
+            Assert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void Example1_Can_reverse_a_word()
+        {
+            var input = new char[] { 't', 'h', 'e' };
+            var expected = new char[] { 't', 'h', 'e' };
+            var sut = new Solution();
+
+            sut.ReverseWords(input);
+
+            Assert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void Example1_Can_reverse_a_letter()
+        {
+            var input = new char[] { 't' };
+            var expected = new char[] { 't' };
+            var sut = new Solution();
+
+            sut.ReverseWords(input);
+
+            Assert.AreEqual(expected, input);
+        }
+    }
+}

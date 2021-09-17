151c5af4ea99735754e943ac29c400812ef27252
Jonathan Buchner <me@jonathanbuchner.com>
2021-09-17 16:48:22 -0700

    Add test for find all numbers

---
+++ leet/LeetCode/Modules/Arrays/FindAllNumbersDisappearedInAnArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.FindAllNumbersDisappearedInAnArray
+{
+    public class Solution
+    {
+        public List<int> FindDisappearedNumbers(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/FindAllNumbersDisappearedInAnArray_tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.Arrays.FindAllNumbersDisappearedInAnArray;
+
+namespace leet_test.LeetCode.Modules.Arrays
+{
+    [TestClass]
+    public class FindAllNumbersDisappearedInAnArray_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            var expected =  new List<int>() { 5, 6 };
+
+            var actual = sut.FindDisappearedNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input = new int[] { 1, 1 };
+            var expected = new List<int>() { 2 };
+
+            var actual = sut.FindDisappearedNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}

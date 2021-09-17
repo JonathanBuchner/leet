a06ed2baa4ffff904c7cd984db53f243411ee233
Jonathan Buchner <me@jonathanbuchner.com>
2021-09-17 14:55:12 -0700

    Add find max consecutive ones

---
+++ leet/LeetCode/Modules/Arrays/MaxConsecutiveOnesII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.MaxConsecutiveOnesII
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var right = 0;
+            var left = 0;
+            var highest = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 1)
+                {
+                    right++;
+                }
+                else
+                {
+                    left = 1 + right;
+                    right = 0;
+                }
+
+                highest = Math.Max(highest, right + left);
+            }
+
+            return highest ;
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/FindMaxConsecutiveOnes_tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Modules.Arrays.MaxConsecutiveOnesII;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Modules.Arrays
+{
+    [TestClass]
+    public class FindMaxConsecutiveOnes_tests
+    {
+        [TestMethod]
+        public void Exmaple1_Simple_Flipping_Only_One_Zero()
+        {
+            var sut = new Solution();
+            var input = new int[] { 1, 0, 1, 1, 0 };
+            var expected = 4;
+
+            var actual = sut.FindMaxConsecutiveOnes(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Exmaple2_Simple_Flipping_Only_One_Zero()
+        {
+            var sut = new Solution();
+            var input = new int[] { 1, 0, 1, 1, 0 };
+            var expected = 4;
+
+            var actual = sut.FindMaxConsecutiveOnes(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { }, 0)]
+        [DataRow(new int[] { 0 }, 1)]
+        [DataRow(new int[] { 0, 0, 0, }, 1)]
+        [DataRow(new int[] { 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1 }, 6)]
+        [DataRow(new int[] { 1, 0,  0, 0,}, 2)]
+        public void Tests_Simple_Flipping_Only_One_Zero(int[] input, int expected)
+        {
+            var sut = new Solution();
+
+            var actual = sut.FindMaxConsecutiveOnes(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

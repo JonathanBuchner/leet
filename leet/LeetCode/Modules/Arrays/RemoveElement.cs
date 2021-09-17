d74bd7c631b38ddf7e26ce2ffe7ebb89d8bb21bb
Jonathan Buchner <me@jonathanbuchner.com>
2021-09-17 13:48:40 -0700

    Add remove element tests

---
+++ leet/LeetCode/Modules/Arrays/RemoveElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.RemoveElement
+{
+    /*
+     * Remove Element
+     * 
+     * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+     * 
+     * https://leetcode.com/explore/featured/card/fun-with-arrays/511/in-place-operations/3575/
+     */
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/RemoveElement_tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.Arrays.RemoveElement;
+
+namespace leet_test.LeetCode.Modules.Arrays
+{
+    [TestClass]
+    public class RemoveElement_tests
+    {
+        [TestMethod]
+        public void Example1_Can_remove_duplicates()
+        {
+            var sut = new Solution();
+            var remove = 3;
+            var input = new int[] { 3, 2, 2, 3 };
+            var expectedArray = new int[] { 2, 2, 2, 3 };
+            var expected = 2;
+
+            var actual = sut.RemoveElement(input, remove);
+
+            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expectedArray, input);
+        }
+        [TestMethod]
+        public void Example2_Can_remove_duplicates_longer()
+        {
+            var sut = new Solution();
+            var remove = 2;
+            var input = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            var expectedArray = new int[] { 0, 1, 4, 0, 3, 0, 4, 2 };
+            var expected = 5;
+
+            var actual = sut.RemoveElement(input, remove);
+
+            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expectedArray, input);
+        }
+        [TestMethod]
+        public void Test_Can_remove_if_no_duplicate()
+        {
+            var sut = new Solution();
+            var remove = 1;
+            var input = new int[] { 2, 3, 4, 5 };
+            var expectedArray = new int[] { 2, 3, 4, 5 };
+            var expected = 0;
+
+            var actual = sut.RemoveElement(input, remove);
+
+            Assert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expectedArray, input);
+        }
+    }
+}

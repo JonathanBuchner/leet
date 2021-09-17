1119058794dacb75f3a6717be9cd9868ba170fcf
Jonathan Buchner <me@jonathanbuchner.com>
2021-09-17 15:17:15 -0700

    Add tests for thirmaxnumber

---
+++ leet/LeetCode/Modules/Arrays/ThirdMaximumNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.Arrays.ThirdMaximumNumber
+{
+    /*
+     * ThirdMaxNumber
+     * 
+     * Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+     * 
+     * https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3231/
+     */
+    public class Solution
+    {
+        public int ThirdMax(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/Arrays/ThirdMaximumNumber_tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.Arrays.ThirdMaximumNumber;
+
+namespace leet_test.LeetCode.Modules.Arrays
+{
+    [TestClass]
+    public class ThirdMaximumNumber_tests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 3, 2, 1}, 1 )]
+        [DataRow(new int[] { 2, 1 }, 1)]
+        [DataRow(new int[] { 1 }, 1)]
+        [DataRow(new int[] { 1, 2, 3, 4}, 2)]
+        [DataRow(new int[] { Int32.MaxValue, 0, -1, Int32.MinValue }, 0)]
+        [DataRow(new int[] { 0, 1, 1, 1, ,1, 1 ,1 }, 1)]
+        public void Examplel1_Basic(int[] input, int expected)
+        {
+            var sut = new Solution();
+
+            var actual = sut.ThirdMax(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

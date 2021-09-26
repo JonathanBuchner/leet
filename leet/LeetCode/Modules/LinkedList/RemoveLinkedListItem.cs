d156a8b5247a6e6ae1c68caac9f9115f4cc9901c
Jonathan Buchner <me@jonathanbuchner.com>
2021-09-26 08:08:40 -0700

    Add RemoveItems

---
+++ leet/LeetCode/Modules/LinkedList/RemoveLinkedListItem.cs
@@ -0,0 +1,47 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.LinkedList.RemoveLinkedListItem
+{
+    /*
+        * Remove Linked List Elements
+        * 
+        * Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+        * 
+        * https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+        */
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+
+            ListNode curr = head;
+            ListNode prev = null;
+
+            while (curr != null)
+            {
+                if (curr.val == val)
+                {
+                    if (prev == null)
+                    {
+                        head = curr.next;
+                    }
+                    else
+                    {
+                        prev.next = curr.next;
+                    }
+                }
+                else
+                {
+                    prev = curr;
+                }
+
+                curr = curr.next;
+            }
+
+            return head;
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/LinkedList/RemoveLinkedListItem_tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Modules.LinkedList.RemoveLinkedListItem;
+using leet_test.Helpers;
+
+namespace leet_test.LeetCode.Modules.LinkedList
+{
+    [TestClass]
+    public class RemoveLinkedListItem_tests
+    {
+        [TestMethod]
+        public void RemoveElement_ShortListWithItemsToRemove_RemovesItems()
+        {
+            var helper = new LinkedListHelper();
+            var sut = new Solution();
+            var input_head = helper.CreateSinglyLinkedList(new int[] { 1, 2, 6, 3, 4, 5, 6 });
+            var input_val = 6;
+            var expected = helper.CreateSinglyLinkedList(new int[] { 1, 2, 3, 4, 5 });
+
+            var actual = sut.RemoveElements(input_head, input_val);
+
+            AssertLinkedList.AreEqualValue(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveElement_FirstItemNeedsRemoval_RemovesItems()
+        {
+            var helper = new LinkedListHelper();
+            var sut = new Solution();
+            var input_head = helper.CreateSinglyLinkedList(new int[] { 6, 1, 2, 6, 3, 4, 5, 6 });
+            var input_val = 6;
+            var expected = helper.CreateSinglyLinkedList(new int[] { 1, 2, 3, 4, 5 });
+
+            var actual = sut.RemoveElements(input_head, input_val);
+
+            AssertLinkedList.AreEqualValue(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveElement_EmptyList_ReturnNull()
+        {
+            var helper = new LinkedListHelper();
+            var sut = new Solution();
+            var input_head = helper.CreateSinglyLinkedList(new int[] { });
+            var input_val = 7;
+            var expected = helper.CreateSinglyLinkedList(new int[] { });
+
+            var actual = sut.RemoveElements(input_head, input_val);
+
+            AssertLinkedList.AreEqualValue(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveElement_ListWhereEveryItemNeedsRemoval_ReturnsNull()
+        {
+            var helper = new LinkedListHelper();
+            var sut = new Solution();
+            var input_head = helper.CreateSinglyLinkedList(new int[] { 7, 7, 7, 7 });
+            var input_val = 7;
+            var expected = helper.CreateSinglyLinkedList(new int[] { });
+
+            var actual = sut.RemoveElements(input_head, input_val);
+
+            AssertLinkedList.AreEqualValue(expected, actual);
+        }
+    }
+}

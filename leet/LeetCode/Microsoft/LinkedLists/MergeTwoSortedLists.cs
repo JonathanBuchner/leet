0461106d20d97505d7d350c5237ba21a81a3ff11
Jonathan Buchner <me@jonathanbuchner.com>
2021-10-22 07:09:51 -0700

    Add tests for MergeTwoSortedLinkedLists

---
+++ leet/LeetCode/Microsoft/LinkedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,22 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.LinkedLists.MergeTwoSortedLists
+{
+    /*
+     * Merge Two Sorted Link Lists
+     * 
+     * Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/32/linked-list/175/
+     */
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/LinkedLists/MergeTwoSortedLists_tests.cs
@@ -0,0 +1,24 @@
+﻿using leet.LeetCode.Microsoft.LinkedLists.MergeTwoSortedLists;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet_test.Helpers;
+
+namespace leet_test.LeetCode.Microsoft.LinkedLists
+{
+    [TestClass]
+    public class MergeTwoSortedLists_tests
+    {
+        [TestMethod]
+        public void MergeTwoLists_CanMergeLists()
+        {
+            var sut = new Solution();
+            var helper_ll= new LinkedListHelper();
+            var input_l1 = helper_ll.CreateSinglyLinkedList(new int[] { 1, 2, 4 });
+            var input_l2 = helper_ll.CreateSinglyLinkedList(new int[] { 1, 3, 4 });
+            var expected = helper_ll.CreateSinglyLinkedList(new int[] { 1, 1, 2, 3, 4, 4 });
+
+            var actual = sut.MergeTwoLists(input_l1, input_l2);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}

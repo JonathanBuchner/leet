10a7a9e9b204d3ad0f7deb620b7a962f7756f0b1
Jonathan Buchner <me@jonathanbuchner.com>
2021-10-22 08:38:43 -0700

    Add answer for MergeKsortedLists

--- leet/LeetCode/Microsoft/LinkedLists/MergeKsortedLists.cs
+++ leet/LeetCode/Microsoft/LinkedLists/MergeKsortedLists.cs
@@ -9,7 +9,58 @@ namespace leet.LeetCode.Microsoft.LinkedLists.MergeKsortedLists
     {
         public ListNode MergeKLists(ListNode[] lists)
         {
-            throw new NotImplementedException();
+            ListNode head = null;
+            ListNode curr = null;
+            ListNode temp = null;
+
+            if (lists.Length == 0) return null;
+
+            while(true)
+            {
+                foreach (var list in lists)
+                {
+                    if (list == null)
+                    {
+                        continue;
+                    }
+                    else if (temp == null)
+                    {
+                        temp = list;
+                    }
+                    else
+                    {
+                        if (temp.val > list.val)
+                        {
+                            temp = list;
+                        }
+                    }
+                }
+
+                if (temp == null)
+                {
+                    break;
+                }
+                else if (head == null)
+                {
+                    head = curr = new ListNode(temp.val);
+                }
+                else
+                {
+                    curr = curr.next = new ListNode(temp.val);
+                }
+
+                for (var i = 0; i < lists.Length; i++)
+                {
+                    if (lists[i] == temp)
+                    {
+                        lists[i] = lists[i].next;
+                        break;
+                    }
+                }
+                temp = null;
+            }
+
+            return head;
         }
     }
 }

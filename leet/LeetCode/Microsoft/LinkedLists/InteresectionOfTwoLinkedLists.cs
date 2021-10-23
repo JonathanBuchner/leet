2039a751d0bed55864e68e9e545e88adac4040e8
Jonathan Buchner <me@jonathanbuchner.com>
2021-10-23 08:44:49 -0700

    Add tests for IntersectionOfTwoLinkedLists

---
+++ leet/LeetCode/Microsoft/LinkedLists/InteresectionOfTwoLinkedLists.cs
@@ -0,0 +1,22 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Microsoft.LinkedLists.InteresectionOfTwoLinkedLists
+{
+    /*
+     * Intersection of Two Linked Lists
+     * 
+     * Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/32/linked-list/212/
+     */
+    public class Solution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Microsoft/LinkedLists/InteresectionOfTwoLinkedLists_tests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Microsoft.LinkedLists.InteresectionOfTwoLinkedLists;
+using leet_test.Helpers;
+using leet.LeetCode.Models;
+
+namespace leet_test.LeetCode.Microsoft.LinkedLists
+{
+    /*
+     * These tests are faking the actual comparison between nodes by comparing the values.
+     */
+    [TestClass]
+    public class InteresectionOfTwoLinkedLists_tests
+    {
+        [TestMethod]
+        public void GetInterectionNode_ListsAreSimilarLength()
+        {
+            var sut = new Solution();
+            var helper = new LinkedListHelper();
+            var input_headA = helper.CreateSinglyLinkedList(new int[] { 4, 1, 8, 4, 5 });
+            var input_headB = helper.CreateSinglyLinkedList(new int[] { 5, 6, 1, 8, 4, 5 });
+            var expected = new ListNode(8);
+
+            var actual = sut.GetIntersectionNode(input_headA, input_headB);
+
+            Assert.AreEqual(expected.val, actual.val);
+        }
+
+        [TestMethod]
+        public void GetInterectionNode_ListsAreDifferentLenghts()
+        {
+            var sut = new Solution();
+            var helper = new LinkedListHelper();
+            var input_headA = helper.CreateSinglyLinkedList(new int[] { 1, 9, 1, 2, 4 });
+            var input_headB = helper.CreateSinglyLinkedList(new int[] { 3, 4, 4 });
+            var expected = new ListNode(8);
+
+            var actual = sut.GetIntersectionNode(input_headA, input_headB);
+
+            Assert.AreEqual(expected.val, actual.val);
+        }
+
+        [TestMethod]
+        public void GetInterectionNode_ListsDontIntersect()
+        {
+            var sut = new Solution();
+            var helper = new LinkedListHelper();
+            var input_headA = helper.CreateSinglyLinkedList(new int[] { 2, 6, 4 });
+            var input_headB = helper.CreateSinglyLinkedList(new int[] { 1, 5 });
+            var expected = new ListNode(8);
+
+            var actual = sut.GetIntersectionNode(input_headA, input_headB);
+
+            Assert.AreEqual(expected.val, actual.val);
+        }
+
+        [TestMethod]
+        public void GetInterectionNode_ListsAreNull()
+        {
+            var sut = new Solution();
+            var helper = new LinkedListHelper();
+            var input_headA = helper.CreateSinglyLinkedList(new int[] {});
+            var input_headB = helper.CreateSinglyLinkedList(new int[] { });
+            var expected = new ListNode(8);
+
+            var actual = sut.GetIntersectionNode(input_headA, input_headB);
+
+            Assert.AreEqual(expected.val, actual.val);
+        }
+
+        [TestMethod]
+        public void GetInterectionNode_AListIsNull()
+        {
+            var sut = new Solution();
+            var helper = new LinkedListHelper();
+            var input_headA = helper.CreateSinglyLinkedList(new int[] { });
+            var input_headB = helper.CreateSinglyLinkedList(new int[] { 5, 6, 1, 8, 4, 5 });
+            var expected = new ListNode(8);
+
+            var actual = sut.GetIntersectionNode(input_headA, input_headB);
+
+            Assert.AreEqual(expected.val, actual.val);
+        }
+    }
+}

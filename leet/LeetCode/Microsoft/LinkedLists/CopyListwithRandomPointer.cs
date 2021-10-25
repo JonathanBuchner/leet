7938b72bb315cd4199709d5923d7c43b5dba2f74
Jonathan Buchner <me@jonathanbuchner.com>
2021-10-25 09:39:10 -0700

    Add CopyRandomList

---
+++ leet/LeetCode/Microsoft/LinkedLists/CopyListwithRandomPointer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Models;
+
+namespace leet.LeetCode.Microsoft.LinkedLists.CopyListwithRandomPointer
+{
+    /*
+     * Copy List with Random Pointer
+     * 
+     * A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+     * 
+     * https://leetcode.com/explore/interview/card/microsoft/32/linked-list/168/
+     */
+    public class Solution
+    {
+        public Node CopyRandomList(Node head)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

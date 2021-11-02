3b20d886ff7db0fa59aef368e06f71040bad1838
Jonathan Buchner <me@jonathanbuchner.com>
2021-11-02 11:22:54 -0700

    Add Group Anagamram and Rate Limiter

---
+++ leet/LeetCode/Modules/HashTables/GroupAnagrams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.HashTables.GroupAnagrams
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Modules/HashTables/GroupAnagrams_tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Modules.HashTables.GroupAnagrams;
+
+namespace leet_test.LeetCode.Modules.HashTables
+{
+    [TestClass]
+    public class GroupAnagrams_tests
+    {
+        [TestMethod]
+        public void GroupAnagrams()
+        {
+            var sut = new Solution();
+            var input = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var expected = new List<List<string>>();
+            expected.Add(new List<string>() { "bat" });
+            expected.Add(new List<string>() { "nat", "tan" });
+            expected.Add(new List<string>() { "ate", "eat", "tea" });
+
+            var actual = sut.GroupAnagrams(input);
+
+            Assert.IsFalse(true);
+        }
+    }
+}

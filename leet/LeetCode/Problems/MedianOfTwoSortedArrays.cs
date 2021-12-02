59c8d059491b3040391a52c736e9d0e2896dc13c
Jonathan Buchner <me@jonathanbuchner.com>
2021-12-02 08:02:20 -0800

    Add tests for Media of Two Sorted Arrays

---
+++ leet/LeetCode/Problems/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.MedianOfTwoSortedArrays
+{
+    /*
+     * Median of Two Sorted Arrays
+     * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+     * 
+     * The overall run time complexity should be O(log (m+n)).
+     * 
+     * https://leetcode.com/problems/median-of-two-sorted-arrays/
+     */
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MedianOfTwoSortedArrays_tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Problems.MedianOfTwoSortedArrays;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MedianOfTwoSortedArrays_tests
+    {
+        [TestMethod]
+        [DataRow(
+            new int[] { 1,3 }, 
+            new int[] { 2 },
+            2.0000d )
+        ]
+        [DataRow(
+            new int[] { 1, 2 },
+            new int[] { 3, 4 },
+            2.5000d)
+        ]
+        [DataRow(
+            new int[] { 0, 0 },
+            new int[] { 0, 0 },
+            0.0000d)
+        ]
+        [DataRow(
+            new int[] { },
+            new int[] { 1 },
+            1.0000d)
+        ]
+        [DataRow(
+            new int[] { 1,},
+            new int[] { 2 },
+            1.5000d)
+        ]
+        public void FindMedianSortedArrays(int[] input_nums1, int[] input_nums2, decimal expected)
+        {
+            var sut = new Solution();
+
+            var actual = sut.FindMedianSortedArrays(input_nums1, input_nums2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

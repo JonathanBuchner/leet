1c05a4031989c2ffe661f318456abca49c56a075
Jonathan Buchner <me@jonathanbuchner.com>
2021-12-02 09:54:48 -0800

    Add tests for ZigZagConversion

---
+++ leet/LeetCode/Problems/ZigzagConversation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.ZigzagConversation
+{
+    /*
+     * Zigzag Conversion
+     * 
+     * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+     * 
+     * https://leetcode.com/problems/zigzag-conversion/
+     */
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

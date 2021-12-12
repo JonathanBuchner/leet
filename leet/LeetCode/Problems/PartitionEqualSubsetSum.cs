6ded72b395dae0340777732c35483a196c6562c0
Jonathan Buchner <me@jonathanbuchner.com>
2021-12-12 08:14:11 -0800

    Add tests for Partition Equal Subset

---
+++ leet/LeetCode/Problems/PartitionEqualSubsetSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.PartitionEqualSubsetSum
+{
+    /*
+     * Partition Equal Subset Sum
+     * 
+     * Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+     * 
+     * https://leetcode.com/problems/partition-equal-subset-sum/
+     */
+    public class Solution
+    {
+        public bool CanPartition(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/PartitionEqualSubsetSum_tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Problems.PartitionEqualSubsetSum;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class PartitionEqualSubsetSum_tests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 5, 11, 5 }, true)]
+        [DataRow(new int[] { 1, 2, 3, 5 }, false)]
+
+        public void CanPartition_Examples(int[] nums, bool expected)
+        {
+            var sut = new Solution();
+
+            var actual = sut.CanPartition(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

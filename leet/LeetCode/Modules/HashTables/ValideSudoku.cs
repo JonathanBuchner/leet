33a14e8fdb3aa3efb438ee536d8180194aaf6b1e
Jonathan Buchner <me@jonathanbuchner.com>
2021-12-12 14:00:35 -0800

    Tests for valid Sudoku

---
+++ leet/LeetCode/Modules/HashTables/ValideSudoku.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Modules.HashTables.ValideSudoku
+{
+    /*
+     * Valid Sudoku
+     * 
+     * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+     * 
+     * https://leetcode.com/explore/learn/card/hash-table/185/hash_table_design_the_key/1126/
+     */
+    public class Solution
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}

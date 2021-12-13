00988b188e991c4c301f372472d6a52c9d4e40b6
Jonathan Buchner <me@jonathanbuchner.com>
2021-12-13 08:49:04 -0800

    Add tests for soncsecutive Characters

---
+++ leet/LeetCode/Problems/ConsecutiveCharacters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.ConsecutiveCharacters
+{
+    public class Solution
+    {
+        /*
+         * Consecutive Characters
+         * 
+         * The power of the string is the maximum length of a non-empty substring that contains only one unique character.  Given a string s, return the power of s.
+         * 
+         * https://leetcode.com/problems/consecutive-characters/
+         */
+        public int MaxPower(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

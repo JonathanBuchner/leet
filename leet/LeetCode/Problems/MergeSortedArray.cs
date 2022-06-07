6034dddbf5cb7c37798e2b848d8f526824e48263
Jonathan Buchner <me@jonathanbuchner>
2022-06-07 10:34:48 -0700

    Add tests for mergesortedarray

---
+++ leet/LeetCode/Problems/MergeSortedArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.MergeSortedArray
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            throw new NotImplementedException();
+            /*var i = 0;
+            var j = 0;
+            var k = -1;
+
+            while (i < m && j < n)
+            {
+                if (k == -1)
+                {
+                    if (nums2[k] < nums2[j]) ;
+                    {
+                        nums1[i] = nums2[k];
+                        k--;
+                    }
+                else
+                    {
+
+                    }
+                }
+                else if (nums1[i] > nums2[j])
+                {
+                    var temp = nums1[i];
+                    nums1[i] = nums2[j];
+                    nums2[j] = temp;
+                    j++;
+                    k++;
+                }
+
+                i++;
+
+            }*/
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MergSortedArray_tests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Problems.MergeSortedArray;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MergSortedArray_tests
+    {
+        [TestMethod]
+        public void CanDoSimpleMerge()
+        {
+            var sut = new Solution();
+            var actual_input_nums1 = new int[]
+            {
+                1, 2, 3, 0, 0, 0
+            };
+            var input_m = 3;
+            var input_nums2 = new int[]
+            {
+                2, 5, 6
+            };
+            var input_n = 3;
+            var expected = new int[]
+            {
+                1,2,2,3,5,6
+            };
+
+            sut.Merge(actual_input_nums1, input_m, input_nums2, input_n);
+
+            Assert.AreEqual(expected, actual_input_nums1);
+        }
+    }
+}

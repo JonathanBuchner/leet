2e82fc0b10dc5712f2d702a5c188dc4e141394b3
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-13 12:07:35 -0700

    Add solution for Pascals Triangle

--- leet/LeetCode/Problems/Pascals_Triangle.cs
+++ leet/LeetCode/Problems/Pascals_Triangle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace leet.LeetCode.Problems.Pascals_Triangle
@@ -8,7 +9,32 @@ namespace leet.LeetCode.Problems.Pascals_Triangle
     {
         public IList<IList<int>> Generate(int numRows)
         {
-            throw new NotImplementedException();
+            var answer = new List<IList<int>>();
+
+            if(numRows > 0)
+            {
+                answer.Add(new List<int>() { 1 });
+            }
+
+            if (numRows > 1)
+            {
+                answer.Add(new List<int>() { 1, 1 });
+            }
+
+            for (var i = 2; i < numRows; i++)
+            {
+                var row = new List<int>() { 1 };
+                
+                for(var k = 1; k < i; k++)
+                {
+                    row.Add(answer[i - 1][k-1] + answer[i - 1][k]);
+                }
+
+                row.Add(1);
+                answer.Add(row);
+            }
+
+            return answer;
         }
     }
 }

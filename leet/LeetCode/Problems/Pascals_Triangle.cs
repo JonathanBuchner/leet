f3f5627780d303cd54049ee77790f3f59f5f9f9d
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-13 11:38:08 -0700

    Add tests for Pascals Triangle

---
+++ leet/LeetCode/Problems/Pascals_Triangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.Pascals_Triangle
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/Pascals_triangle_tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Problems.Pascals_Triangle;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class Pascals_triangle_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+
+        }
+    }
+}

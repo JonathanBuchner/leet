9f77a21b3136a29e98d5a1be04c2f5fb86723b44
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-14 10:06:54 -0700

    Incorrect solution for delete operation for two strings.

--- leet/LeetCode/Problems/Delete_operation_two_strings.cs
+++ leet/LeetCode/Problems/Delete_operation_two_strings.cs
@@ -8,7 +8,34 @@ namespace leet.LeetCode.Problems.Delete_operation_two_strings
     {
         public int MinDistance(string word1, string word2)
         {
-            throw new NotImplementedException();
+            var substring = word1.Length > word2.Length ? word2.Length : word1.Length;
+
+            while (substring > 0)
+            {
+                var list = new HashSet<string>();
+                var p1 = 0;
+
+                while (p1 + substring <= word1.Length)
+                {
+                    list.Add(word1.Substring(p1, substring));
+                    p1++;
+                }
+
+                p1 = 0;
+
+                while (p1+substring <= word2.Length)
+                {
+                    if(list.Contains(word2.Substring(p1, substring)))
+                    {
+                        return (word1.Length - substring) + (word2.Length - substring); 
+                    };
+                    p1++;
+                }
+
+                substring--;
+            }
+
+            return word1.Length + word2.Length;
         }
     }
 }

0f4e4961e793fec32172b0ebabf58c517e20e179
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-14 08:31:02 -0700

    Add tests for Delete operation for Two Strings

---
+++ leet/LeetCode/Problems/Delete_operation_two_strings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.Delete_operation_two_strings
+{
+    public class Solution
+    {
+        public int MinDistance(string word1, string word2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/Delete_operation_two_strings_tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using leet.LeetCode.Problems.Delete_operation_two_strings;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class Delete_operation_two_strings_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_word1 = "sea";
+            var input_word2 = "eat";
+            var expected = 2;
+
+            var actual = sut.MinDistance(input_word1, input_word2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_word1 = "leetcode";
+            var input_word2 = "etco";
+            var expected = 4;
+
+            var actual = sut.MinDistance(input_word1, input_word2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

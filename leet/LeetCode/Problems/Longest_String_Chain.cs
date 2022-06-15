6aa771677a735c41475921e4164ba43b13be5e58
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-15 08:56:55 -0700

    Add tests for longest string chain

---
+++ leet/LeetCode/Problems/Longest_String_Chain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.Longest_String_Chain
+{
+    public class Solution
+    {
+        public int LongestStrChain(string[] words)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

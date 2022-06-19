3e9b9770ff293c1696aea8d7616242439356a953
Jonathan Buchner <me@jonathanbuchner>
2022-06-18 18:43:35 -0700

    Add tests for Search Suggestions System.

---
+++ leet/LeetCode/Problems/SearchSuggestionSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.SearchSuggestionSystem
+{
+    public class Solution
+    {
+        public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

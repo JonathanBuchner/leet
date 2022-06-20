6661a8af9d4144c357589961ade822aada49d508
Jonathan Buchner <me@jonathanbuchner>
2022-06-20 08:47:46 -0700

    Add short encoding of words problem

---
+++ leet/LeetCode/Problems/ShortEncodingOfWords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet.LeetCode.Problems.ShortEncodingOfWords
+{
+    public class Solution
+    {
+        public int MinimumLengthEncoding(string[] words)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/ShortEncodingOfWords_tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leet.LeetCode.Problems.ShortEncodingOfWords;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class ShortEncodingOfWords_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input = new string[]
+            {
+                "time", "me", "bell"
+            };
+            var expected = 10;
+
+            var actual = sut.MinimumLengthEncoding(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input = new string[]
+            {
+                "t"
+            };
+            var expected = 2;
+
+            var actual = sut.MinimumLengthEncoding(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

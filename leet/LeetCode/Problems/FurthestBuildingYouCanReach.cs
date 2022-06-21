d03c3b470e2141bd0c367b9090bef8195e95629b
Jonathan Buchner <me@jonathanbuchner>
2022-06-21 11:52:45 -0700

    Second solut to FurthestBridge.  Time limit exceeded.

--- leet/LeetCode/Problems/FurthestBuildingYouCanReach.cs
+++ leet/LeetCode/Problems/FurthestBuildingYouCanReach.cs
@@ -7,59 +7,42 @@ namespace leet.LeetCode.Problems.FurthestBuildingYouCanReach
 {
     public class Solution
     {
-        private int[] heights;
-        private int bricks;
-        private int ladders;
         public int FurthestBuilding(int[] heights, int bricks, int ladders)
         {
-            this.heights = heights;
-            this.bricks = bricks;
-            this.ladders = ladders;
+            var highests = new List<int>();
+            var furthest = 0;
+            var usedBricks = 0;
 
-            var buldingsTraverse = 0;
-
-            var index = heights.Length / 2;
-
-            return buildingTraverse;
-        }
-
-        private bool Furthest(int index, out int lastIndex)
-        {
-            if(index)
-
-            var climbs = GetClimbList(0, index);
-
-            return CanTraverse(climbs);
-        }
-
-        private List<int> GetClimbList(int indexLeft, int indexRight)
-        {
-            var list = new List<int>();
-
-            for (int i = indexLeft; i < heights.Length - 1 && i <= indexRight; i++)
+            for (var i = 1; i < heights.Length; i++)
             {
-                var diff = heights[i + 1] - heights[i];
-                if(diff > 0)
+                var diff = heights[i] - heights[i - 1];
+                
+                if (diff > 0)
                 {
-                    list.Add(diff);
+                    highests.Add(diff);
+
+                    if(highests.Count > ladders)
+                    {
+                        var lowest = highests[0];
+                        for (var k = 1; k < highests.Count; k++)
+                        {
+                            lowest = Math.Min(lowest, highests[k]);
+                        }
+                        highests.Remove(lowest);
+
+                        usedBricks += lowest;
+
+                        if (usedBricks > bricks)
+                        {
+                            break;
+                        }
+                    }
                 }
+
+                furthest = i;
             }
 
-            list.Sort( (a,b) => b.CompareTo(a));
-
-            return list;
-        }
-
-        private bool CanTraverse(List<int> climbs)
-        {
-            if(climbs.Count <= ladders)
-            {
-                return true;
-            }
-            climbs.RemoveRange(0, ladders);
-            var bricksNeeded = climbs.Sum( c => c);
-
-            return bricks >= bricksNeeded;
+            return furthest;
         }
     }
 }

525174fd7b1302a2cc3df791da19b4bb6c758a95
Jonathan Buchner <me@jonathanbuchner>
2022-06-22 08:41:26 -0700

    Add inorder and preorder traversal for binary trees

--- leet/LeetCode/Modules/BinaryTree/BinaryTree.md
+++ leet/LeetCode/Modules/BinaryTree/BinaryTree.md
@@ -3,3 +3,9 @@ https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/
 
 #### Pre-order Traversal
 Pre-order traversal is to visit the root first. Then traverse the left subtree. Finally, traverse the right subtree
+
+#### In-order Traversal
+In-order traversal is to traverse the left subtree first. Then visit the root. Finally, traverse the right subtree.
+
+#### Post-order Traversal
+Post-order traversal is to traverse the left subtree first. Then traverse the right subtree. Finally, visit the root.
---
+++ leet/LeetCode/Modules/BinaryTree/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,38 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Modules.BinaryTree.BinaryTreeInorderTraversal
+{
+    /*
+     * Binary Tree Inorder Traversal
+     * 
+     * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+     * 
+     * https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+     */
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+
+            Dfa(root, list);
+
+            return list;
+        }
+
+        private void Dfa(TreeNode node, List<int> list)
+        {
+            if (node != null)
+            {
+                Dfa(node.left, list);
+                list.Add(node.val);
+                Dfa(node.right, list);
+            }
+        }
+    }
+}

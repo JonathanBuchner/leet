1d723bb6cef1f380815e5f08e6f0b88e639a6b36
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-23 19:10:55 -0700

    Add problem Constructed target array with multiple sums

---
+++ leet/LeetCode/Problems/ConstructTargetArrayWithMultipleSums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ConstructTargetArrayWithMultipleSums
+{
+    /*
+     * Construct Target Array With Multiple Sums
+     * 
+     * You are given an array target of n integers. From a starting array arr consisting of n 1's, you may perform the following procedure :
+
+     *   let x be the sum of all elements currently in your array.
+choose index i, such that 0 <= i < n and set the value of arr at index i to x.
+
+     * You may repeat this procedure as many times as needed.
+Return true if it is possible to construct the target array from arr, otherwise, return false.
+     * 
+     * https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+     */
+    public class Solution
+    {
+        public bool IsPossible(int[] target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/ConstructTargetArrayWithMultipleSums_tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.ConstructTargetArrayWithMultipleSums;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class ConstructTargetArrayWithMultipleSums_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_target = new int[]
+            {
+                9,3,5
+            };
+            var expected = true;
+
+            var actual = sut.IsPossible(input_target);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_target = new int[]
+            {
+                1,1,1,2
+            };
+            var expected = false;
+
+            var actual = sut.IsPossible(input_target);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var input_target = new int[]
+            {
+                8,5
+            };
+            var expected = true;
+
+            var actual = sut.IsPossible(input_target);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

02dde163f4dc34218e35cb12c12d6c2c8284b90e
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-24 19:37:10 -0700

    Ass problem non decreasing array

---
+++ leet/LeetCode/Problems/NonDecreasingArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.NonDecreasingArray
+{
+    /*
+     * Non-decreasing Array
+     * 
+     * Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+     * 
+     * https://leetcode.com/problems/non-decreasing-array/
+     */
+    public class Solution
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

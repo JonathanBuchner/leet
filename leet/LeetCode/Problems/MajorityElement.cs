7e4dcc0377d948f82376e583a2c658ae2d99dca5
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-27 09:21:15 -0700

    Add solution to majority element

---
+++ leet/LeetCode/Problems/MajorityElement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MajorityElement
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            var count = 0;
+            var highNum = 0;
+
+            if (nums.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be empty.");
+            }
+
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    ++dict[num];
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+
+            foreach (var entry in dict)
+            {
+                if (entry.Value > count)
+                {
+                    count = entry.Value;
+                    highNum = entry.Key;
+                }
+            }
+
+            return highNum;
+        }
+    }
+}

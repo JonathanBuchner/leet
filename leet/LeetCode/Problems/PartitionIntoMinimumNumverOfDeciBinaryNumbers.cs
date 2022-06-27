b67f0433f5cfd2b52bba2e5a8c12d26216476408
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-27 08:36:06 -0700

    Add probem Partition into minimum number of deci binary

---
+++ leet/LeetCode/Problems/PartitionIntoMinimumNumverOfDeciBinaryNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.PartitionIntoMinimumNumverOfDeciBinaryNumbers
+{
+    /*
+     * Partitioning Into Minimum Number Of Deci-Binary Numbers
+     * 
+     * A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+     * 
+     * 
+     * https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+     */
+    public class Solution
+    {
+        public int MinPartitions(string n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/PartitionIntoMinimumNumverOfDeciBinaryNumbers_tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.PartitionIntoMinimumNumverOfDeciBinaryNumbers;
+
+namespace leet_test.LeetCode.Problems
+{
+
+    [TestClass]
+    public class PartitionIntoMinimumNumverOfDeciBinaryNumbers_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_num = "32";
+            var expected = 3;
+
+            var actual = sut.MinPartitions(input_num);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

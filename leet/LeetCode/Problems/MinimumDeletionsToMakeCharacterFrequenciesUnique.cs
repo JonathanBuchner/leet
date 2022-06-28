161249b55b9f82971379c8d42ef65b180a676d5d
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-27 17:30:20 -0700

    Add problem minumum dletions to make character frequencies unique

---
+++ leet/LeetCode/Problems/MinimumDeletionsToMakeCharacterFrequenciesUnique.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumDeletionsToMakeCharacterFrequenciesUnique
+{
+    /*
+     * Minimum Deletions to Make Character Frequencies Unique
+     * 
+     * A string s is called good if there are no two different characters in s that have the same frequency.  Given a string s, return the minimum number of characters you need to delete to make s good.
+     * 
+     * https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+     */
+    public class Solution
+    {
+        public int MinDeletions(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MinimumDeletionsToMakeCharacterFrequenciesUnique_tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinimumDeletionsToMakeCharacterFrequenciesUnique;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumDeletionsToMakeCharacterFrequenciesUnique_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_string = "aab";
+            var expected = 0;
+
+            var actual = sut.MinDeletions(input_string);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_string = "aaabbbcc";
+            var expected = 2;
+
+            var actual = sut.MinDeletions(input_string);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var input_string = "ceabaacb";
+            var expected = 2;
+
+            var actual = sut.MinDeletions(input_string);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

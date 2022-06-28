1db659a4913cbe5f42603564b240454c13ff6afa
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-28 10:04:20 -0700

    Add problem excel sheet column number

---
+++ leet/LeetCode/Problems/ExcelSheetColumnNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ExcelSheetColumnNumber
+{
+    /*
+     * Excel Sheet Column Number
+     * 
+     * Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number
+     * 
+     * https://leetcode.com/problems/excel-sheet-column-number/
+     */
+    public class Solution
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

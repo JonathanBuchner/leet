a3603b3397ca30d5e316b9fed623bf48db83ec1a
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-29 12:19:48 -0700

    Add solution to invert Binary Tree

---
+++ leet/LeetCode/Problems/InvertBinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.InvertBinaryTree
+{
+    /*
+     * Invert Binary Tree
+     * 
+     * Given the root of a binary tree, invert the tree, and return its root.
+     * 
+     * https://leetcode.com/problems/invert-binary-tree/
+     */
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            var temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            return root;
+        }
+    }
+}

2dae219efd820650cd99bcb349f2e76913d3c596
Jonathan Buchner <me@jonathanbuchner.com>
2022-06-29 17:33:50 -0700

    Add problem Minimum Moves to Equal Array Elements II

---
+++ leet/LeetCode/Problems/MinimumMovestoEqualArrayElementsII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumMovestoEqualArrayElementsII
+{
+    /*
+     * Minimum Moves to Equal Array Elements II
+     * 
+     * Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.  In one move, you can increment or decrement an element of the array by 1.
+     * 
+     * https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+     */
+    public class Solution
+    {
+        public int MinMoves2(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MinimumMovestoEqualArrayElementsII_tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinimumMovestoEqualArrayElementsII;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumMovestoEqualArrayElementsII_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_nums = new int[]
+            {
+                1,2,3
+            };
+            var expected = 2;
+                
+            var actual = sut.MinMoves2(input_nums);
+
+            Assert.AreEqual(expected, actual);  
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_nums = new int[]
+            {
+                1,10,2,9
+            };
+            var expected = 16;
+
+            var actual = sut.MinMoves2(input_nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

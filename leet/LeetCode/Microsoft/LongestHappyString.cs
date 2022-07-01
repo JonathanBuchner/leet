35907118ad52ae9cd1e2ca1fb3e84c7d256d1836
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-01 10:27:10 -0700

    First try for longest happy string

--- leet/LeetCode/Microsoft/LongestHappyString.cs
+++ leet/LeetCode/Microsoft/LongestHappyString.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 
 namespace leet.LeetCode.Microsoft.LongestHappyString
@@ -13,9 +17,126 @@ namespace leet.LeetCode.Microsoft.LongestHappyString
       */
     public class Solution
     {
+        private int A;
+        private int B;
+        private int C;
+        private char prev;
+        private StringBuilder sb = new StringBuilder();
+
         public string LongestDiverseString(int a, int b, int c)
         {
-            throw new NotImplementedException();
+            A = a;
+            B = b;
+            C = c;
+
+            prev = A < B && A < C ?
+                'a' :
+                B < C ?
+                    'b' :
+                    'c'; 
+                    
+
+            while (A > 0 || B > 0 || C > 0)
+            {
+                if(prev == 'a')
+                {
+                    if (B > C)
+                    {
+                        HandleCase('b');
+
+                    }
+                    else
+                    {
+                        HandleCase('c');
+                    }
+                } 
+                else if (prev == 'b')
+                {
+                    if (A > C)
+                    {
+                        HandleCase('a');
+                    }
+                    else
+                    {
+                        HandleCase('c');
+                    }
+                } 
+                else
+                {
+                    if (A > B)
+                    {
+                        HandleCase('a');
+                    }
+                    else
+                    {
+                        HandleCase('b');
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private void HandleCase(char character)
+        {
+            switch(character)
+            {
+                case 'a':
+                    var am = Math.Min(2, A);
+
+                    if (am == 1)
+                    {
+                        sb.Append('a');
+                        A--;
+                    }
+                    else if (am == 2)
+                    {
+                        sb.Append("aa");
+                        A -= 2;
+                    }
+
+                    prev = 'a';
+                    break;
+                case 'b':
+                    
+                    var bm = Math.Min(2, B);
+
+                    if (bm == 1)
+                    {
+                        sb.Append('b');
+                        --B;
+                    }
+                    else if (bm == 2)
+                    {
+                        sb.Append("bb");
+                        B -= 2;
+                    }
+
+                    prev = 'b';
+                    break;
+
+                case 'c':
+                    var cm = Math.Min(2, C);
+
+                    if (cm == 1)
+                    {
+                        sb.Append('c');
+                        C--;
+                    }
+                    else if (cm == 2)
+                    {
+                        sb.Append("cc");
+                        C -= 2;
+                    }
+                    prev = 'c';
+
+                    break;
+
+                default:
+
+                    throw new ArgumentException("Must provide a character");
+                   
+            }
         }
     }
 }

84a0d861782bf986864a3c9afccf68ed2df9521e
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-01 09:11:17 -0700

    Add Binary Tree Help for building binary trees

--- leet_test/Helpers/BinaryTreeHelper.cs
+++ leet_test/Helpers/BinaryTreeHelper.cs
@@ -5,47 +5,62 @@ using System.Text;
 
 namespace leet_test.Helpers
 {
-    /*public class BinaryTreeHelper
+    public static class BinaryTreeHelper
     {
-        private int?[] arr;
-        public BinaryTreeHelper(int?[] arr)
+        public static TreeNode MakeTree(int?[] arr)
         {
-            this.arr = arr;
-            
-        }
-        public TreeNode MakeTree(int index = 0)
-        {
-            if (arr.Length == 0 || arr[0] == null)
+            var queue = new Queue<TreeNode>();
+            var i = 0;
+
+            if(arr == null || arr.Length == 0 || arr[i] == null)
             {
                 return null;
             }
-            else
+
+            var root = new TreeNode((int)arr[i]);
+            i++;
+
+            queue.Enqueue(root);
+
+            while(queue.Count != 0)
             {
-                var node = new TreeNode((int)arr[index]);
+                var curr = queue.Dequeue();
+
+                if (i >= arr.Length)
+                {
+                    curr.left = null;
+                    curr.right = null;
+                }
+                else if(i == arr.Length - 1)
+                {
+                    curr.left = GetNode(arr[i]);
+                    curr.right = null;
+                    i++;
+                } 
+                else
+                {
+                    curr.left = GetNode(arr[i]);
+                    curr.right = GetNode(arr[i+1]);
+                    i += 2;
+                    
+                    if(curr.left != null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+
+                    if (curr.right != null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }
+                }
             }
 
-            index++;
-            if (index <= arr.Length)
-            {
-
-            }
-
+            return root;
         }
 
-        private TreeNode MakeNode(int index)
+        private static TreeNode GetNode(int? val)
         {
-            if (index >= arr.Length)
-            {
-                return null;
-            }
-            else if (arr[index] == null)
-            {
-                return null;
-            }
-            else
-            {
-                return new TreeNode((int)arr[index]);
-            }
+            return val == null ? null : new TreeNode((int)val);
         }
-    }*/
+    }
 }

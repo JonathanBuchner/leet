4412a8c49335d62a724a96d52b85a3fb612cdfae
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-01 20:33:05 -0700

    Add problem MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts

---
+++ leet/LeetCode/Problems/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts
+{
+    /*
+     * Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+     * 
+     * Return the maximum area of a piece of cake after you cut at each horizontal and vertical position provided in the arrays horizontalCuts and verticalCuts
+     * 
+     * https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
+     */
+    public class Solution
+    {
+        public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts_tests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_h = 5;
+            var input_w = 4;
+            var input_horizontalCuts = new int[]
+            {
+                1,2,4
+            };
+            var input_verticalCuts = new int[]
+            {
+                1,3
+            };
+            var expected = 4;
+
+            var actual = sut.MaxArea(input_h, input_w, input_horizontalCuts, input_verticalCuts);
+        
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_h = 5;
+            var input_w = 4;
+            var input_horizontalCuts = new int[]
+            {
+                3,1
+            };
+            var input_verticalCuts = new int[]
+            {
+                1
+            };
+            var expected = 6;
+
+            var actual = sut.MaxArea(input_h, input_w, input_horizontalCuts, input_verticalCuts);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var input_h = 5;
+            var input_w = 4;
+            var input_horizontalCuts = new int[]
+            {
+                3
+            };
+            var input_verticalCuts = new int[]
+            {
+                3
+            };
+            var expected = 9;
+
+            var actual = sut.MaxArea(input_h, input_w, input_horizontalCuts, input_verticalCuts);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

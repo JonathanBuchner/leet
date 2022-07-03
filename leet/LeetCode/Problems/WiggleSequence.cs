a9887fb6393cb51269232fd37b5d7720329602b9
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-03 10:29:49 -0700

    Add problem Wiggle Sequence

---
+++ leet/LeetCode/Problems/WiggleSequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.WiggleSequence
+{
+    /*
+     * Wiggle Subsequence
+     * 
+     * A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
+     * 
+     * Given an integer array nums, return the length of the longest wiggle subsequence of nums.
+     * 
+     * https://leetcode.com/problems/wiggle-subsequence/
+     */
+    public class Solution
+    {
+        public int WiggleMaxLength(int[] nums)
+        {
+              throw new NotImplementedException();
+        }
+    }
+}

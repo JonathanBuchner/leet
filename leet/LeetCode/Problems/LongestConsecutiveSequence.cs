1e9b2b7c7a7b03ed75078ea4518ef81359b28c0c
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-05 07:52:56 -0700

    Add problem Longest Consecutive  Sequence

---
+++ leet/LeetCode/Problems/LongestConsecutiveSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.LongestConsecutiveSequence
+{
+    /*
+     * Longest Consecutive Sequence
+     * 
+     * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence. You must write an algorithm that runs in O(n) time.
+     * 
+     * https://leetcode.com/problems/longest-consecutive-sequence/
+     */
+    public class Solution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

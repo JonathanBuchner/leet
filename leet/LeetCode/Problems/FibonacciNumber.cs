f1c48f5c118b3b1ed926a86726c3bb3cf434b724
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-05 17:51:42 -0700

    First solution to fibonacci number

---
+++ leet/LeetCode/Problems/FibonacciNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.FibonacciNumber
+{
+    /*
+     * Fibonacci Number
+     * 
+     * Given n, calculate F(n).
+     * 
+     * https://leetcode.com/problems/fibonacci-number/
+     */
+    public class Solution
+    {
+        public int Fib(int n)
+        {
+            var arr = new int[n + 1];
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            arr[0] = 0;
+            arr[1] = 1;
+
+            for (var i = 2; i <= n; ++i)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+
+            return arr[n];
+        }
+    }
+}

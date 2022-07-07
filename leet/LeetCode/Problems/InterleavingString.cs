c642d3c0c7c932f9c7289036cdb094da35ab5afd
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-06 17:54:42 -0700

    Add problem Interleaving String

---
+++ leet/LeetCode/Problems/InterleavingString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.InterleavingString
+{
+    /*
+     * Interleaving String
+     * 
+     * iven strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2. An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings 
+     * 
+     * https://leetcode.com/problems/interleaving-string/
+     */
+    public class Solution
+    {
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/InterleavingString_tests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.InterleavingString;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class InterleavingString_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var input_s1 = "aabcc";
+            var input_s2 = "dbbca";
+            var input_s3 = "aadbbcbcac";
+            var expected = true;
+
+            var actual = sut.IsInterleave(input_s1, input_s2, input_s3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var input_s1 = "aabcc";
+            var input_s2 = "dbbca";
+            var input_s3 = "aadbbbaccc";
+            var expected = false;
+
+            var actual = sut.IsInterleave(input_s1, input_s2, input_s3);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var input_s1 = "";
+            var input_s2 = "";
+            var input_s3 = "";
+            var expected = true;
+
+            var actual = sut.IsInterleave(input_s1, input_s2, input_s3);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

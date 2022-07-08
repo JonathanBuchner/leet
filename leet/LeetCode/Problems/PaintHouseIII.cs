7ba7ecd886193324596ec68d3b2085dda813ee91
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-08 09:10:15 -0700

    Add problem painted house

---
+++ leet/LeetCode/Problems/PaintHouseIII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.PaintHouseIII
+{
+    /*
+     * Paint House III
+     * 
+     * Return the minimum cost of painting all the remaining houses in such a way that there are exactly target neighborhoods. If it is not possible, return -1
+     * 
+     * https://leetcode.com/problems/paint-house-iii/
+     */
+    public class Solution
+    {
+        public int MinCost(int[] houses, int[][] cost, int m, int n, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/PaintHouseIII_tests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.PaintHouseIII;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class PaintHouseIII_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var houses = new int[]
+            {
+                0,0,0,0,0
+            };
+            var costs = new int[][]
+            {
+                //[1,10],[10,1],[10,1],[1,10],[5,1]
+                new int[]{ 1, 10},
+                new int[]{ 10, 1},
+                new int[]{ 10, 1},
+                new int[]{ 1, 10},
+                new int[]{ 5, 1},
+            };
+            var rows = 5;
+            var colors = 2;
+            var target = 3;
+            var expected = 9;
+
+            var actual = sut.MinCost(houses, costs, rows, colors, target);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var houses = new int[]
+            {
+                0,2,1,2,0
+            };
+            var costs = new int[][]
+            {
+                // [1,10],[10,1],[10,1],[1,10],[5,1]
+                new int[]{ 1, 10},
+                new int[]{ 10, 1},
+                new int[]{ 10, 1},
+                new int[]{ 1, 10},
+                new int[]{ 5, 1},
+            };
+            var rows = 5;
+            var colors = 2;
+            var target = 3;
+            var expected = 11;
+
+            var actual = sut.MinCost(houses, costs, rows, colors, target);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var houses = new int[]
+            {
+                3,1,2,3
+            };
+            var costs = new int[][]
+            {
+                // [1,1,1],[1,1,1],[1,1,1],[1,1,1]
+                new int[]{ 1, 1, 1},
+                new int[]{ 1, 1, 1},
+                new int[]{ 1, 1, 1},
+                new int[]{ 1, 1, 1},
+            };
+            var rows = 4;
+            var colors = 3;
+            var target = 3;
+            var expected = -1;
+
+            var actual = sut.MinCost(houses, costs, rows, colors, target);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

68faffa9d9851fc763f316e294d7474d2c4629e9
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-11 09:21:18 -0700

    Add problem Min Cost Climbing Stairs

---
+++ leet/LeetCode/Problems/MinCostCLimbingStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinCostCLimbingStairs
+{
+    /*
+     * Min Cost Climbing Stairs
+     * 
+     * You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps
+     * 
+     * https://leetcode.com/problems/min-cost-climbing-stairs/
+     */
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MinCostCLimbingStairs_tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinCostCLimbingStairs;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinCostCLimbingStairs_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var cost = new int[]
+            {
+                10,15,20
+            };
+            var expected = 15;
+
+            var actual = sut.MinCostClimbingStairs(cost);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var cost = new int[]
+            {
+                1,100,1,1,1,100,1,1,100,1
+            };
+            var expected = 6;
+
+            var actual = sut.MinCostClimbingStairs(cost);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

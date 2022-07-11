23149cd5bc68412896aea381fffc43dd4b0a074f
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-11 08:42:16 -0700

    Add problem Binary Tree Right Side View

---
+++ leet/LeetCode/Problems/BinaryTreeRightSideView.cs
@@ -0,0 +1,24 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.BinaryTreeRightSideView
+{
+    /*
+     * Binary Tree Right Side View
+     * 
+     * Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+     * 
+     * https://leetcode.com/problems/binary-tree-right-side-view/
+     */
+    public class Solution
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}

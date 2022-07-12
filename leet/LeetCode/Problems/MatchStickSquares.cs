868300428375aac006d0853e53d51b024aa2a35d
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-12 07:42:22 -0700

    Add problem matchstick squares

---
+++ leet/LeetCode/Problems/MatchStickSquares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MatchstickToSquares
+{
+    /*
+     * Matchsticks to Square
+     * 
+     * You are   given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. You want to use all the matchsticks to make one square. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.  Return true if you can make this square and false otherwise.
+     * 
+     * https://leetcode.com/problems/matchsticks-to-square/
+     */
+    public class Solution
+    {
+        public bool Makesquare(int[] matchsticks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/MathstickToSquares_tests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MatchstickToSquares;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MathstickToSquares_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var matchsticks = new int[]
+            {
+                1,1,2,2,2
+            };
+            var expected = true;
+
+            var actual = sut.Makesquare(matchsticks);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var matchsticks = new int[]
+            {
+                3,3,3,3,4
+            };
+            var expected = false;
+
+            var actual = sut.Makesquare(matchsticks);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

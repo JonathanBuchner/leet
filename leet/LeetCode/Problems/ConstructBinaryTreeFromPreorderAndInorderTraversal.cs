e493c09de4a0894cc21a31aa71dab8d2e6408cf7
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-13 17:25:28 -0700

    Add problem Construct Binary Tree From Preorder And Inorder Traversal

---
+++ leet/LeetCode/Problems/ConstructBinaryTreeFromPreorderAndInorderTraversal.cs
@@ -0,0 +1,24 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ConstructBinaryTreeFromPreorderAndInorderTraversal
+{
+    /*
+     * Construct Binary Tree from Preorder and Inorder Traversal
+     * 
+     * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+     * 
+     * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+     */
+    public class Solution
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

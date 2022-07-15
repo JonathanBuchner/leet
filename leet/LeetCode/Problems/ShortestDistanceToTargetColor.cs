2e67d0e7bc29d9697badc01a93efe594f732522d
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-15 06:22:22 -0700

    Add problem shortest distance to target color

---
+++ leet/LeetCode/Problems/ShortestDistanceToTargetColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ShortestDistanceToTargetColor
+{
+    /*
+     * Shortest Distance to Target Color
+     * 
+     * You are given an array colors, in which there are three colors: 1, 2 and 3.  You are also given some queries. Each query consists of two integers i and c, return the shortest distance between the given index i and the target color c. If there is no solution return -1.
+     * 
+     * https://leetcode.com/problems/shortest-distance-to-target-color/
+     */
+    public class Solution
+    {
+        public IList<int> ShortestDistanceColor(int[] colors, int[][] queries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

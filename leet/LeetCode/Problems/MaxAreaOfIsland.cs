c15101b4cf55c23f2360fc6333974c0cece3d8ef
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-14 18:59:12 -0700

    Add problem max area of island

---
+++ leet/LeetCode/Problems/MaxAreaOfIsland.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MaxAreaOfIsland
+{
+    /*
+     * Max Area of Island
+     * 
+     * Return the maximum area of an island in grid. If there is no island, return 0.
+     * 
+     * https://leetcode.com/problems/max-area-of-island/
+     */
+    public class Solution
+    {
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

cf4b66c757a2417263647c47b492808a3c353be2
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-16 08:35:29 -0700

    Add solution to out of boundar paths.  Does not do modulo

---
+++ leet/LeetCode/Problems/OutOfBoundaryPaths.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.OutOfBoundaryPaths
+{
+    /*
+     * Out of Boundary Paths
+     * 
+     * There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. You are allowed to move the ball to one of the four adjacent cells in the grid (possibly out of the grid crossing the grid boundary). You can apply at most maxMove moves to the ball. Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
+     * 
+     * https://leetcode.com/problems/out-of-boundary-paths/
+     */
+    public class Solution
+    {
+        public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+        {
+            // If no squares or jumps
+            if(m == 0 || n == 0 || maxMove == 0)
+            {
+                return 0;
+            }
+
+            // If one square
+            if(m == 1 && n == 1)
+            {
+                return 4;
+            }
+
+            var score = 0;
+            var memo = new int[m, n];
+            memo[startRow, startColumn] = 1;
+
+            for (var i = 0; i < maxMove; i++)
+            {
+                var temp = new int[m, n];
+                for (var x = 0; x < m; ++x)
+                {
+                    for (var y = 0; y < n; ++y)
+                    {
+                        var balls = memo[x, y];
+
+                        if (x == 0)
+                        {
+                            score += balls;
+                        }
+                        else
+                        {
+                            temp[x - 1, y] += balls;
+                        }
+
+                        if (y == 0)
+                        {
+                            score += balls;
+                        }
+                        else
+                        {
+                            temp[x, y - 1] += balls;
+                        }
+
+                        if (x == m - 1)
+                        {
+                            score += balls;
+                        }
+                        else
+                        {
+                            temp[x + 1, y] += balls;
+                        }
+
+                        if (y == n - 1)
+                        {
+                            score += balls;
+                        }
+                        else
+                        {
+                            temp[x, y + 1] += balls;
+                        }
+
+                    }
+                }
+                memo = temp;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/OutOfBoundaryPaths_tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.OutOfBoundaryPaths;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class OutOfBoundaryPaths_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var m = 2;
+            var n = 2;
+            var maxMoves = 2;
+            var startRow = 0;
+            var startColumn = 0;
+            var expected = 6;
+
+            var actual = sut.FindPaths(m, n, maxMoves, startRow, startColumn);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var m = 1;
+            var n = 3;
+            var maxMoves = 3;
+            var startRow = 0;
+            var startColumn = 1;
+            var expected = 12;
+
+            var actual = sut.FindPaths(m, n, maxMoves, startRow, startColumn);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

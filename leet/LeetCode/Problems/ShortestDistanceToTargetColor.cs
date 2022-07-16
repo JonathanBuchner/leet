aa3d6354366ffd343d4b82cbc22369a6986f3919
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-15 18:36:12 -0700

    Add solution to distance to target color

--- leet/LeetCode/Problems/ShortestDistanceToTargetColor.cs
+++ leet/LeetCode/Problems/ShortestDistanceToTargetColor.cs
@@ -17,29 +17,76 @@ namespace leet.LeetCode.Problems.ShortestDistanceToTargetColor
     {
         public IList<int> ShortestDistanceColor(int[] colors, int[][] queries)
         {
-            throw new NotImplementedException();
-
-            var dp = new int[ 3, colors.Length];           
+            var dp = new int[3, colors.Length];           
             var list = new List<int>();
           
-            var i0 = 0;
-            var i1 = 0;
-            var i2 = 0;
+            var color = new int[3];
+
+            for (int i = 0; i < color.Length; i++)
+            {
+                color[i] = Int32.MaxValue;
+            }
 
             for(var i = 0; i < colors.Length; ++i)
             {
-                var c = colors[i];
+                
+                for(var k = 0; k < color.Length; ++k)
+                {
+                    if(colors[i] - 1 == k)
+                    {
+                        color[k] = 0;
+                    }
+                    else if(color[k] != Int32.MaxValue)
+                    {
+                        color[k]++;
+                    }
+                }
 
-
+                for(var k = 0; k < color.Length; ++k)
+                {
+                    dp[k, i] = color[k];
+                }
             }
 
-            for (var i = colors.Length; i >= 0; --i)
+            for (int i = 0; i < color.Length; i++)
+            {
+                color[i] = Int32.MaxValue;
+            }
+
+            for (var i = colors.Length - 1; i >= 0; --i)
             {
 
+                for (var k = 0; k < color.Length; ++k)
+                {
+                    if (colors[i] - 1 == k)
+                    {
+                        color[k] = 0;
+                    }
+                    else if (color[k] != Int32.MaxValue)
+                    {
+                        color[k]++;
+                    }
+                }
+
+                for (var k = 0; k < color.Length; ++k)
+                {
+                    dp[k, i] = Math.Min(dp[k, i], color[k]);
+                }
             }
+            
 
             for (var i = 0; i < queries.Length; ++i)
             {
+                var dist = dp[queries[i][1] - 1, queries[i][0]];
+
+                if(dist == Int32.MaxValue)
+                {
+                    list.Add(-1);
+                }
+                else
+                {
+                    list.Add(dist);
+                }
                 
             }
 

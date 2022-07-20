4373cd3d7528672ed637c7d442db0c1dee724893
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-20 09:06:30 -0700

    Add problem K inverse pairs array

---
+++ leet/LeetCode/Problems/KInversePairsArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.KInversePairsArray
+{
+    /*
+     * K Inverse Pairs Array
+     * 
+     * For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j]. Given two integers n and k, return the number of different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs. Since the answer can be huge, return it modulo 109 + 7.
+     * 
+     * https://leetcode.com/problems/k-inverse-pairs-array/
+     */
+    public class Solution
+    {
+        public int KInversePairs(int n, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

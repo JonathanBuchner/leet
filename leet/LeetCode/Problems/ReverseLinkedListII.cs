fb0b0655856e09f944cb669d36ed2a5e3e73660c
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-21 06:54:05 -0700

    Add problem reverse linked list 2

---
+++ leet/LeetCode/Problems/ReverseLinkedListII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ReverseLinkedListII
+{
+    /*
+     * Reverse Linked List II
+     * 
+     * Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+     * 
+     * https://leetcode.com/problems/reverse-linked-list-ii/
+     */
+    public class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

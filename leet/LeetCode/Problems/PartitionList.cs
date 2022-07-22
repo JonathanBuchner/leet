62a5219b57581816a12ba7dfba56aecd9cb79bb2
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-21 19:59:11 -0700

    Add problem partition list

---
+++ leet/LeetCode/Problems/PartitionList.cs
@@ -0,0 +1,24 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.PartitionList
+{
+    /*
+     * Partition List
+     * 
+     * Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.  You should preserve the original relative order of the nodes in each of the two partitions.
+     * 
+     * https://leetcode.com/problems/partition-list/
+     */
+    public class Solution
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            throw new NotImplementedException();    
+        }
+    }
+}

699d1642bba5f21410c13c5c363cf6983522334a
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-22 07:55:20 -0700

    Add answr for find pivot index

--- leet/LeetCode/Modules/Arrays/FindPivotIndex.cs
+++ leet/LeetCode/Modules/Arrays/FindPivotIndex.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace leet.LeetCode.Modules.Arrays
+namespace leet.LeetCode.Modules.Arrays.FindPivotIndex
 {
-    internal class FindPivotIndex
+    /*
+     * Find Pivot Index
+     * 
+     * Given an array of integers nums, calculate the pivot index of this array.
+     * 
+     * https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+     */
+    public class Solution
     {
+        public int PivotIndex(int[] nums)
+        {
+
+            var left = 0;
+            var right = nums.Sum();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i > 0)
+                {
+                    left += nums[i - 1];
+                }
+
+                right -= nums[i];
+
+                if (left == right)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
     }
 }

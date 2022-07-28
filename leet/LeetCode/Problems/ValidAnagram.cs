4c26e449d0864b4a27ff62febd99eb59c27c713b
Jonathan Buchner <me@jonathanbuchner.com>
2022-07-28 07:21:45 -0700

    Add solution to valid anagram

---
+++ leet/LeetCode/Problems/ValidAnagram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ValidAnagram
+{
+    /*
+     * Valid Anagram
+     * 
+     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+     * 
+     * https://leetcode.com/problems/valid-anagram/
+     */
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+
+            var lets = new int[26];
+
+            for (var i = 0; i < s.Length; ++i)
+            {
+                lets[s[i] - 'a']++;
+            }
+
+            for (var i = 0; i < t.Length; ++i)
+            {
+                lets[t[i] - 'a']--;
+            }
+
+            for (var i = 0; i < lets.Length; ++i)
+            {
+                if (lets[i] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

a1241a6304f7e02a41bbea22dd188e38c7f63ded
Jonathan Buchner <me@jonathanbuchner.com>
2022-08-08 08:40:53 -0700

    Add solution to longest increasing sub sequence

---
+++ leet/LeetCode/Problems/LongestIncreasingSubsequence.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.LongestIncreasingSubsequence
+{
+    /*
+     * Longest Increasing Subsequence
+     * 
+     * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+     * 
+     * https://leetcode.com/problems/longest-increasing-subsequence/
+     */
+    public class Solution
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            var list = new List<int>();
+            list.Add(nums[0]);
+
+            for(var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > list[list.Count - 1])
+                {
+                    list.Add(nums[i]);
+                }
+                else
+                {
+                    ReplaceLowest(list, nums[i]);
+                }
+            }
+
+            return list.Count;
+        }
+
+        public void ReplaceLowest(List<int> list, int num)
+        {
+            var left = 0;
+            var right = list.Count - 1;
+
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+
+                if (list[mid] == num)
+                {
+                    return;
+                }
+
+                if (list[mid] > num)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+            }
+
+            list[left] = num;
+        }
+    }
+}
---
+++ leet_test/LeetCode/Problems/LongestIncreasingSubsequence_tests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.LongestIncreasingSubsequence;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class LongestIncreasingSubsequence_tests
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            var sut = new Solution();
+            var nums = new int[]
+            {
+                10,9,2,5,3,7,101,18
+            };
+            var expected = 4;
+
+            var actual = sut.LengthOfLIS(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            var sut = new Solution();
+            var nums = new int[]
+            {
+               0,1,0,3,2,3
+            };
+            var expected = 4;
+
+            var actual = sut.LengthOfLIS(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Example3()
+        {
+            var sut = new Solution();
+            var nums = new int[]
+            {
+               7,7,7,7,7,7,7
+            };
+            var expected = 1;
+
+            var actual = sut.LengthOfLIS(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MyCase()
+        {
+            var sut = new Solution();
+            var nums = new int[]
+            {
+               10, 9 , 8, 7 , 6 , 6 , 7, 8, 10, 8, 6, 4, 1 , 2, 3, 4, 5, 6, 7, 8, 9 
+            };
+            var expected = 9;
+
+            var actual = sut.LengthOfLIS(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

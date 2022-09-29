5ad2965f4f2b1af612a2672221a127fbff42b00c
Jonathan Buchner <me@jonathanbuchner.com>
2022-09-29 08:28:14 -0700

    Add solution to find k closest elements

---
+++ leet/LeetCode/Problems/BinaryTreeWithFactors.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.BinaryTreeWithFactors
+{
+    /*
+     * 823. Binary Trees With Factors
+     * 
+     * Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
+     * 
+     * https://leetcode.com/problems/binary-trees-with-factors/
+     */
+    public class Solution
+    {
+        public int NumFactoredBinaryTrees(int[] arr)
+        {
+            var dp = new int[arr.Length];
+            var dict = new Dictionary<int, int>();
+            int mod = 100000000;
+            Array.Sort(arr);
+
+            for(var i = 0; i < arr.Length; i++)
+            {
+                dict.Add(arr[i], i);
+                dp[i] = 1;
+            }
+
+            for(var i = 0; i < arr.Length; i++)
+            {
+                for(var k = i; k < arr.Length; k++)
+                {
+                    var total = arr[i] * arr[k];
+
+                    if(total > arr[arr.Length] - 1)
+                    {
+                        break;
+                    }
+
+                    if(dict.ContainsKey(total))
+                    {
+                        if(k == i)
+                        {
+                            arr[total] += dp[i];
+                        }
+                        else
+                        {
+                            arr[total] += dp[i] * dp[k];
+                        }
+                    }
+                }
+            }
+
+            return (int) dp.Sum() % mod;
+        }
+    }
+}
---
+++ leet/LeetCode/Problems/FindKClosestElements.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.FindKClosestElements
+{
+    /*
+     * Find K Closests Elements
+     * 
+     * Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+     * 
+     * https://leetcode.com/problems/find-k-closest-elements/
+     */
+    public class Solution
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            var prev = 0;
+            var next = 0;
+            var result = new int[k];
+
+            var i = 0;
+            while (i < arr.Length && arr[i] < x)
+            {
+                i++;
+            }
+            prev = i - 1;
+            next = i;
+
+            while (prev >= 0 && next < arr.Length && k > 0)
+            {
+                if (arr[next] - x < x - arr[prev])
+                {
+                    next++;
+                }
+                else
+                {
+                    prev--;
+                }
+                k--;
+            }
+
+            while (prev >= 0 && k > 0)
+            {
+                prev--;
+                k--;
+            }
+
+            while (next < arr.Length && k > 0)
+            {
+                next++;
+                k--;
+            }
+
+            prev++;
+
+            Array.Copy(arr, prev, result, 0, result.Length);
+
+            return result;
+        }
+    }
+}

2cb15c1f86cc4599b8e3543c73a5cb8a6ce6463a
Jonathan Buchner <me@jonathanbuchner.com>
2022-12-14 08:35:36 -0800

    Add solution to Meeting Rooms II.

---
+++ leet/LeetCode/Problems/MeetingRoomsII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MeetingRoomsII
+{
+    public class Solution
+    {
+        public bool CanAttendMeetings(int[][] intervals)
+        {
+            var list = new List<int[]>();
+
+            foreach (var entry in intervals)
+            {
+                list.Add(entry);
+            }
+
+            list.Sort((a, b) => a[0].CompareTo(b[0]));
+
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i][1] > list[i + 1][0])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

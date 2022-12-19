f96dbcef2cff366007e10ae3dc79dd6b21e3f2cd
Jonathan Buchner <me@jonathanbuchner.com>
2022-12-19 08:30:21 -0800

    Add solution to add strings.

---
+++ leet/LeetCode/Problems/AddStrings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.AddStrings
+{
+    public class Solution
+    {
+        public string AddStrings(string num1, string num2)
+        {
+            var sb = new StringBuilder();
+            var n1 = num1.Length - 1;
+            var n2 = num2.Length - 1;
+            var carry = 0;
+
+            try
+            {
+                while (n1 >= 0 && n2 >= 0)
+                {
+                    var c1 = Char.GetNumericValue(num1[n1]);
+                    var c2 = Char.GetNumericValue(num2[n2]);
+
+                    var total = c1 + c2 + carry;
+                    sb.Append(total % 10);
+
+                    carry = total > 9 ? 1 : 0;
+
+                    n1--;
+                    n2--;
+                }
+
+                while (n1 >= 0)
+                {
+                    var c1 = Char.GetNumericValue(num1[n1]);
+                    var total = c1 + carry;
+
+                    sb.Append(total % 10);
+                    carry = total > 9 ? 1 : 0;
+
+                    n1--;
+                }
+
+                while (n2 >= 0)
+                {
+                    var c2 = Char.GetNumericValue(num2[n2]);
+                    var total = c2 + carry;
+
+                    sb.Append(total % 10);
+                    carry = total > 9 ? 1 : 0;
+
+                    n2--;
+                }
+
+                if (carry == 1)
+                {
+                    sb.Append('1');
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return new String(sb.ToString().Reverse().ToArray());
+        }
+    }
+}

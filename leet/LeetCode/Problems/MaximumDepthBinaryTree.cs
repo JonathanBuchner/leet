a0b17c048de9c6e5a6a46def1a7dab663e701d95
Jonathan Buchner <me@jonathanbuchner.com>
2023-02-15 18:55:51 -0800

    Add solution to maximim depth binary tree.

---
+++ leet/LeetCode/Problems/MaximumDepthBinaryTree.cs
@@ -0,0 +1,33 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MaximumDepthBinaryTree
+{
+    /*
+     * Maximum Depth of Binary Tree
+     * 
+     * Given the root of a binary tree, return its maximum depth. A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+     * 
+     * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+     */
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var left = MaxDepth(root.left) + 1;
+            var right = MaxDepth(root.right) + 1;
+
+            return left > right ? left : right;
+        }
+    }
+}

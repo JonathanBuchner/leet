0de63c6511c471fef33a7b8f29f411c6cc51b5ee
Jonathan Buchner <me@jonathanbuchner.com>
2023-02-21 17:48:55 -0800

    Add solution to CapacityToShipWithinDDays.

---
+++ leet/LeetCode/Problems/CapacityToShipPackagesWithinDDays.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.CapacityToShipPackagesWithinDDays
+{
+    /*
+     * Capacity To Ship Packages Within D Days
+     * 
+     * Conveyor belt has packages that must be shipped from one port to another within days days.
+     * 
+     * The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+     * 
+     * Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+     * 
+     * https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+     */
+    public class Solution
+    {
+        private int[] weights;
+        private int days;
+
+        public bool Feasability(int capacity)
+        {
+            var neededDays = 1;
+            var current = 0;
+
+            for (var i = 0; i < weights.Length; i++)
+            {
+                current += weights[i];
+
+                if (current > capacity)
+                {
+                    neededDays++;
+                    current = weights[i];
+                }
+            }
+
+            return neededDays <= days;
+        }
+
+        public int ShipWithinDays(int[] weights, int days)
+        {
+            this.days = days;
+            this.weights = weights;
+            var total = 0;
+            var max = 0;
+
+            for (var i = 0; i < weights.Length; i++)
+            {
+                total += weights[i];
+                max = Math.Max(max, weights[i]);
+            }
+
+            while (max < total)
+            {
+                var weight = (max + total) / 2;
+
+                if (Feasability(weight))
+                {
+                    total = weight;
+                }
+                else
+                {
+                    max = weight + 1;
+                }
+            }
+
+            return max;
+
+        }
+    }
+}
--- leet/LeetCode/Problems/SentenceSimilarity.cs
+++ leet/LeetCode/Problems/SentenceSimilarity.cs
@@ -4,9 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace leet.LeetCode.Problems
+namespace leet.LeetCode.Problems.SentenceSimilarity
 {
-    internal class SentenceSimilarity
+    /*
+     * Sentence Similarity
+     * 
+     * We can represent a sentence as an array of words, for example, the sentence "I am happy with leetcode" can be represented as arr = ["I","am",happy","with","leetcode"].
+     * 
+     * Given two sentences sentence1 and sentence2 each represented as a string array and given an array of string pairs similarPairs where similarPairs[i] = [xi, yi] indicates that the two words xi and yi are similar.
+     * 
+     * Return true if sentence1 and sentence2 are similar, or false if they are not similar.
+     * 
+     * Sentences are similar if:
+     * They have the same length (i.e., the same number of words)
+     * sentence1[i] and sentence2[i] are similar.
+     * 
+     * Notice that a word is always similar to itself, also notice that the similarity relation is not transitive. For example, if the words a and b are similar, and the words b and c are similar, a and c are not necessarily similar.
+     * 
+     * https://leetcode.com/problems/sentence-similarity/
+     */
+    public class Solution
     {
+        public bool AreSentencesSimilar(string[] sentence1, string[] sentence2, IList<IList<string>> similarPairs)
+        {
+            if (sentence1.Length != sentence2.Length)
+            {
+                return false;
+            }
+
+            var dict = new Dictionary<string, string>();
+
+            for (var i = 0; i < similarPairs.Count; i++)
+            {
+                dict.Add(similarPairs[i][0], similarPairs[i][1]);
+            }
+
+            for (var i = 0; i < sentence1.Length; i++)
+            {
+                if (sentence1[i] == sentence2[i])
+                {
+                    continue;
+                }
+
+
+                if (dict.ContainsKey(sentence1[i]))
+                {
+                    if (dict[sentence1[i]] == sentence2[i])
+                    {
+                        continue;
+                    }
+                }
+
+                if (dict.ContainsKey(sentence2[i]))
+                {
+                    if (dict[sentence2[i]] == sentence1[i])
+                    {
+                        continue;
+                    }
+                }
+
+                return false;
+            }
+
+            return true;
+        }
     }
 }

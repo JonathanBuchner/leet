1f4f1bc7e4d1a07841c40614bde358ee7d853837
Jonathan Buchner <me@jonathanbuchner.com>
2023-04-19 07:29:56 -0700

    Add solution for power of three.

---
+++ leet/LeetCode/Problems/PowerOfThree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.PowerOfThree
+{
+    /*
+     * 326. Power of Three
+     * 
+     * Given an integer n, return true if it is a power of three. Otherwise, return false.
+     * 
+     * An integer n is a power of three, if there exists an integer x such that n == 3x.
+     * 
+     * https://leetcode.com/problems/power-of-three/description/
+     */
+    public class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n < 1)
+            {
+                return false;
+            }
+
+            if (n == 1)
+            {
+                return true;
+            }
+
+            while (n % 3 == 0)
+            {
+
+                if (n == 3)
+                {
+                    return true;
+                }
+
+                n /= 3;
+            }
+
+            return false;
+        }
+    }
+}

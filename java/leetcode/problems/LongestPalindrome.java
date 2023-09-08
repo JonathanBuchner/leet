0846f01a7fb8a98fe3d9a993b4c780a781bb12d0
Jonathan Buchner <me@jonathanbuchner.com>
2023-09-08 07:55:31 -0700

    Add Java.

---
+++ java/leetcode/problems/LongestPalindrome.java
@@ -0,0 +1,39 @@
+/*
+* Longest Palindrome
+* 
+* @author Jonathan Buchner
+* 
+* Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+*
+* https://leetcode.com/problems/longest-palindrome/description/
+*/
+
+import java.util.HashSet; 
+
+class Solution {
+    public int longestPalindrome(String s) {
+        var hashset = new HashSet<Character>();
+        var length = 0;
+
+        for (var i = 0; i < s.length(); i++)
+        {
+            if (hashset.contains(s.charAt(i)))
+            {
+                length += 2;
+                hashset.remove(s.charAt(i));
+            }
+            else
+            {
+                hashset.add(s.charAt(i));
+            }
+        }
+
+        // If there is at least one remaining char that can be used in the middle
+        if (s.length() > length)
+        {
+            length++;
+        }
+        
+        return length;
+    }
+}

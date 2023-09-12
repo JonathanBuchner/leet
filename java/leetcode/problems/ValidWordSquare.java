643d6403a6b8b3b546b1f75bf6276732afc74cce
Jonathan Buchner <me@jonathanbuchner.com>
2023-09-12 13:38:05 -0700

    Add solution to Teemo Attacking.

--- java/leetcode/problems/ValidWordSquare.java
+++ java/leetcode/problems/ValidWordSquare.java
@@ -1,3 +1,28 @@
+/**
+ * Valid word square
+ * 
+ * Given an array of strings words, return true if it forms a valid word square.
+ * 
+ * A sequence of strings forms a valid word square if the kth row and column read the same string, where 0 <= k < max(numRows, numColumns).
+ * 
+ * https://leetcode.com/problems/valid-word-square/
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ValidWordSquare
+{
+    public static void main(String[] args)
+    {
+        List<String> testinput = new ArrayList<String>(Arrays.asList("abcd","bnrt","crmy","dtye"));
+        var test1 = new Solution().validWordSquare(testinput);
+        System.out.println(test1);
+    }
+}
+ 
+
 // In progress
 class Solution {
     public boolean validWordSquare(List<String> words) {

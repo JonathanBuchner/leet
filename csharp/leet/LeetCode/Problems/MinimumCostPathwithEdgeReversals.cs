2e27919e326bd6738ce8b8041119b7b9c5a75abe
Jonathan Buchner <me@jonathanbuchner.com>
2026-01-27 17:59:16 -0800

    Add today's programming assignment.

---
+++ csharp/leet/LeetCode/Problems/MinimumCostPathwithEdgeReversals.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumCostPathwithEdgeReversals
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-cost-path-with-edge-reversals/description/?envType=daily-question&envId=2026-01-27
+    /// You are given a directed, weighted graph with n nodes labeled from 0 to n - 1, and an array edges where edges[i] = [ui, vi, wi] represents a directed edge from node ui to node vi with cost wi
+    /// 
+    /// Each node ui has a switch that can be used at most once: when you arrive at ui and have not yet used its switch, you may activate it on one of its incoming edges vi → ui reverse that edge to ui → vi and immediately traverse it
+    /// 
+    /// The reversal is only valid for that single move, and using a reversed edge costs 2 * wi
+    /// 
+    /// Return the minimum total cost to travel from node 0 to node n - 1. If it is not possible, return -1.
+    /// 
+    /// </summary>
+    public class Solution
+    {
+        public int MinCost(int n, int[][] edges)
+        {
+            var list = new List<IList<int>>();
+
+            for (var i = 0; i < edges.Length; i++)
+            {
+                list.Add(edges[i].ToList());
+                list.Add(new List<int>() {
+                    edges[i][1],
+                    edges[i][0],
+                    edges[i][2] * 2
+                });
+            }
+
+            var frontier = new PriorityQueue<IList<int>, int>();
+            foreach (var item in list)
+            {
+                if (item[0] == 0)
+                {
+                    frontier.Enqueue(item, item[2]);
+                }
+            }
+
+            list.RemoveAll(i => i[0] == 0);
+
+            while (frontier.Count != 0)
+            {
+                var curr = frontier.Dequeue();
+                
+                if (curr[1] == n-1)
+                {
+                    return curr[2];
+                }
+
+                foreach (var item in list)
+                {
+                    if (curr[1] == item[0])
+                    {
+                        item[2] += curr[2];
+                        frontier.Enqueue(item, item[2]);
+                    }
+                }
+
+                list.RemoveAll(i => i[0] == curr[1]);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/MinimumCostPathwithEdgeReversals_tests.cs
@@ -0,0 +1,64 @@
+﻿using leet_test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinimumCostPathwithEdgeReversals;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumCostPathwithEdgeReversals_tests
+    {
+        public static IEnumerable<object[]> Cases()
+        {
+            // Case 1
+            yield return new object[] {
+                ( 4,
+                new int[][] {
+                    new int[] { 0, 1, 3 },
+                    new int[] { 3, 1, 1 },
+                    new int[] { 2, 3, 4 },
+                    new int[] { 0, 2, 2 },
+                }),
+                5
+            };
+
+            // Case 2
+            yield return new object[] {
+                (4,
+                new int[][] {
+                    new int[] { 0, 2, 1 },
+                    new int[] { 2, 1, 1 },
+                    new int[] { 1, 3, 1 },
+                    new int[] { 2, 3, 3 },
+                }),
+                3
+            };
+
+            // Case 113
+            yield return new object[] {
+                (4,
+                new int[][] {
+                    new int[] { 2,3,25 },
+                    new int[] { 2,1,18 },
+                    new int[] { 3,1,2 },
+                }),
+                -1
+            };
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Cases), DynamicDataSourceType.Method)]
+        public void MinimumPairRemoval_tests_basic((int n, int[][] edges) input, int expected)
+        {
+            var sut = new Solution();
+
+            var actual = sut.MinCost(input.n, input.edges);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

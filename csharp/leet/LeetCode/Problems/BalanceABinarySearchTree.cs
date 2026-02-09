fd694bf74440ef519ebab0997238792b74675a28
Jonathan Buchner <me@jonathanbuchner.com>
2026-02-09 10:47:32 -0800

    Remove failing tests for old problems.  Add tests for BalanceBinaryTreeTests.

---
+++ csharp/leet/LeetCode/Problems/BalanceABinarySearchTree.cs
@@ -0,0 +1,17 @@
+﻿using leet.LeetCode.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.BalanceABinarySearchTree
+{
+    public class Solution
+    {
+        public TreeNode BalanceBST(TreeNode root)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/BalanceABinarySearchTree_tests.cs
@@ -0,0 +1,39 @@
+﻿using leet.LeetCode.Models;
+using leet_test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using leet.LeetCode.Problems.BalanceABinarySearchTree;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class BalanceABinarySearchTree_tests
+    {
+        public static IEnumerable<(TreeNode input, TreeNode expected)> Cases()
+        {
+            yield return (
+                BinaryTreeHelper.MakeTree(new int?[] { 1, null, 2, null, 3, null, 4, null, null }),
+                BinaryTreeHelper.MakeTree(new int?[] { 2, 1, 3, null, null, null, 4 })
+            );
+
+            yield return (
+                BinaryTreeHelper.MakeTree(new int?[] { 2, 1, 3 }),
+                BinaryTreeHelper.MakeTree(new int?[] { 2, 1, 3 })
+            );  
+        }
+
+
+        [DataTestMethod]
+        [DynamicData(nameof(Cases), DynamicDataSourceType.Method)]
+        public void BalanceBinaryTreeTests(TreeNode input, TreeNode expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.BalanceBST(input);
+
+            AssertBinaryTrees.AreEqualValue(expected, actual);
+        }
+
+    }
+}

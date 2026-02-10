3784f75345850cb565f86480b4976b55adfb0889
Jonathan Buchner <me@jonathanbuchner.com>
2026-02-10 09:58:09 -0800

    Add test cases for Longest Balanced Subarray I

---
+++ csharp/leet/LeetCode/Problems/LongestBalancedSubarray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.LongestBalancedSubarray
+{
+    public class Solution
+    {
+        public int LongestBalanced(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/LongestBalancedSubarry1_tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.LongestBalancedSubarray;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class LongestBalancedSubarry1_tests
+    {
+        private static IEnumerable<(int[] input, int expected)> cases()
+        {
+            yield return (
+                    new int[] {2, 5, 4, 3},
+                    4
+                );
+
+            yield return (
+                    new int[] { 3, 2, 2, 5, 4 },
+                    5
+                );
+
+            yield return (
+                    new int[] { 1, 2, 3, 2 },
+                    3
+                );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void LongestBalancedSubarry1_test123(int[] input, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.LongestBalanced(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

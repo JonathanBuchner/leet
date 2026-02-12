15edff1a4f79488cb38470cc84edc125a0f63c7e
Jonathan Buchner <me@jonathanbuchner.com>
2026-02-12 08:36:39 -0800

    Add test cases for Longest Balanced Substring 1

---
+++ csharp/leet/LeetCode/Problems/LongestBalancedSubstring1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.LongestBalancedSubstring1
+{
+    public class Solution
+    {
+        public int LongestBalanced(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/LongestBalancedSubstring1_tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.LongestBalancedSubstring1;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class LongestBalancedSubstring1_tests
+    {
+        private static IEnumerable<(string input, int expected)> get_test_data()
+        {
+            // Test Case 1: Example 1 is simple long string
+            yield return (
+                    "abbac",
+                    4
+                );
+
+            // Test Case 2: Example 2 simple middle substring
+            yield return (
+                    "zzabccy",
+                    4
+                );
+
+            // Test Case 3: Example 3 handle odd number
+            yield return (
+                    "aba",
+                    2
+                );
+
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(get_test_data), DynamicDataSourceType.Method)]
+        public void LongestBalancedSubstring1_basic_tests(string input, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.LongestBalanced(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

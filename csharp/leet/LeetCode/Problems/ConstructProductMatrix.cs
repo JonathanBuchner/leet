364ed4e05e88cf58dd27edf92d1a33a5b6bc9771
Jonathan Buchner <me@jonathanbuchner.com>
2026-03-24 10:41:50 -0700

    Add tests for Construct Product Matrix

---
+++ csharp/leet/LeetCode/Problems/ConstructProductMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ConstructProductMatrix
+{
+    public class Solution
+    {
+        public int[][] ConstructProductMatrix(int[][] grid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/ConstructProductMatrix_tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.ConstructProductMatrix;
+using leet_test.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class ConstructProductMatrix_tests
+    {
+        private static IEnumerable<object> cases()
+        {
+            yield return (
+                new int[][]
+                {
+                    [1,2],[3,4]
+                },
+                new int[][]
+                {
+                    [24,12],[8,6]
+                }
+            );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void ConstructProductMatrix_basic_tests(int[][] input, int[][] expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.ConstructProductMatrix(input);
+
+            AssertTwoDemensionalArrays.AreEqual(expected, actual);
+        }
+    }
+}

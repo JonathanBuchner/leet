0a3771c634cfdb4bb29f9c8f30c467bc92209576
Jonathan Buchner <me@jonathanbuchner.com>
2026-03-26 07:19:36 -0700

    Add tests for Equal sum grid partition II

---
+++ csharp/leet/LeetCode/Problems/EqualSumGridPartitionII.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.EqualSumGridPartitionII
+{
+    public class Solution
+    {
+        public bool CanPartitionGrid(int[][] grid)
+        {
+            throw new NotImplementedException("Waiting for testing set up.");
+        }
+        
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/EqualSumGridPartitionII_tests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.EqualSumGridPartitionII;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class EqualSumGridPartitionII_tests
+    {
+        private static IEnumerable<object> cases()
+        {
+            yield return (
+                new int[][]
+                {
+                    [1,4],[2,3]
+                },
+                true
+            );
+
+
+            yield return (
+                new int[][]
+                {
+                    [1,2],[3,4]
+                },
+                false
+            );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void EqualSumGridPartitionII_basic_tests(int[][] input, bool expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.CanPartitionGrid(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

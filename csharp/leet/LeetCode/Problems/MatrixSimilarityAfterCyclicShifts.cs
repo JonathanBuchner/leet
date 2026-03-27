34a56f03a085bd14b486ebc597498ec8c4b73abd
Jonathan Buchner <me@jonathanbuchner.com>
2026-03-27 09:42:17 -0700

    Improve solution to matrix simulairty after cycles shift.

--- csharp/leet/LeetCode/Problems/MatrixSimilarityAfterCyclicShifts.cs
+++ csharp/leet/LeetCode/Problems/MatrixSimilarityAfterCyclicShifts.cs
@@ -11,19 +11,14 @@ namespace leet.LeetCode.Problems.MatrixSimilarityAfterCyclicShifts
     {
         public bool AreSimilar(int[][] mat, int k)
         {
-            // Shortcut for cycle such that before and and after index is same after all cycles.
             k %= mat[0].Length;
             if (mat.Length == 0)
                 return true;
             
             for (var n = 0; n < mat.Length; n++)
-            {
                 for (var m = 0; m < mat[n].Length; m++)
-                {
                     if (mat[n][m] != mat[n][(m + k) % mat[n].Length])
                         return false;
-                }
-            }
 
             return true;
         }

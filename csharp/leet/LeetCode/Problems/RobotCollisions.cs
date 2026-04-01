7c7c5fcac9b86f3cdeb66546c4a680703334ccfb
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-01 10:18:01 -0700

    Add tests for survive tobot health.

---
+++ csharp/leet/LeetCode/Problems/RobotCollisions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.RobotCollisions
+{
+    public class Solution
+    {
+        public IList<int> SurvivedRobotsHealths(int[] positions, int[] healths, string directions)
+        {
+            throw new NotImplementedException("Need to add tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/RobotCollisions_tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.RobotCollisions;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class RobotCollisions_tests
+    {
+        private static IEnumerable<((int[] positions, int[] healths, string directions), int[] expected)> cases()
+        {
+            yield return (
+                (
+                    new int[] { 5, 4, 3, 2, 1 }, 
+                    new int[] { 2, 17, 9, 15, 10 },
+                    "RRRRR"
+                ),
+                new int[] { 2, 17, 9, 15, 10 }     
+            );
+
+            yield return (
+                (
+                    new int[] { 3, 5, 2, 6 },
+                    new int[] { 10, 10, 15, 12 },
+                    "RLRL"
+                ),
+                new int[] { 14 }
+            );
+
+            yield return (
+                (
+                    new int[] { 1, 2, 5, 6 },
+                    new int[] { 10, 10, 11, 11 },
+                    "RLRL"
+                ),
+                new int[] { }
+            );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void RobotCollisions_basic_tests((int[] positions, int[] healths, string directions) input, int[] expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.SurvivedRobotsHealths(input.positions, input.healths, input.directions);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

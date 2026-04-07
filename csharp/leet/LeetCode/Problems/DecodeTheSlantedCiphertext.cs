cb3dc1d78ffd0f3a97fef59c69b48dc37bdfb941
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-07 10:50:33 -0700

    Add tsts for decode ciphertext.

---
+++ csharp/leet/LeetCode/Problems/DecodeTheSlantedCiphertext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.DecodeTheSlantedCiphertext
+{
+    public class Solution
+    {
+        public string DecodeCiphertext(string encodedText, int rows)
+        {
+            throw new NotImplementedException("Waiting to add tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/DecodeTheSlantedCiphertext_tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.DecodeTheSlantedCiphertext;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class DecodeTheSlantedCiphertext_tests
+    {
+        private static IEnumerable<(string encodedText, int rows, string expected)> cases()
+        {
+            yield return (
+                "ch   ie   pr",
+                3,
+                "cipher"
+            );
+
+            yield return (
+               "iveo    eed   l te   olc",
+               4,
+               "i love leetcode"
+           );
+
+            yield return (
+               "coding",
+               1,
+               "coding"
+           );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void DecodeTheSlantedCiphertext_basic_tests(string encodedText, int rows, string expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.DecodeCiphertext(encodedText, rows);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

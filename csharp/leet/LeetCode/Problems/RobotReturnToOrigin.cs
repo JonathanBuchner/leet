76f3a7b477eb8db8ad59acb30250424b8a21e53b
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-07 10:36:21 -0700

    Add solution to robot return to origin.

---
+++ csharp/leet/LeetCode/Problems/RobotReturnToOrigin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.RobotReturnToOrigin
+{
+    public class Solution
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0, y = 0;
+
+            foreach (char move in moves)
+            {
+                switch (move)
+                {
+                    case 'L':
+                        y--;
+                        break;
+                    case 'R':
+                        y++;
+                        break;
+                    case 'U':
+                        x--;
+                        break;
+                    case 'D':
+                        x++;
+                        break;
+                }
+            }
+
+            if (x == 0 && y == 0) return true;
+
+            return false;
+        }
+    }
+}

e52860ad8a489010c3a3a46d35486d861158c926
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-07 10:09:14 -0700

    Their solution to Walking robot simulation 2

--- csharp/leet/LeetCode/Problems/WalkingRobotSimulationII.cs
+++ csharp/leet/LeetCode/Problems/WalkingRobotSimulationII.cs
@@ -14,132 +14,60 @@ namespace leet.LeetCode.Problems.WalkingRobotSimulationII
 {
     public class Robot
     {
-        private int x;
-        private int y;
-        private int width;
-        private int height;
-        private Direction dir;
-        private int mod;
-
+        private bool moved = false;
+        private int idx = 0;
+        private List<int[]> pos = new List<int[]>();
+        private List<int> dir = new List<int>();
+        private Dictionary<int, string> toDir = new Dictionary<int, string>();
 
         public Robot(int width, int height)
         {
-            this.width = width - 1;
-            this.height = height - 1;
-            dir = Direction.East;
-            x = 0;
-            y = 0;
-            mod = GetMod(width, height);
-        }
+            toDir[0] = "East";
+            toDir[1] = "North";
+            toDir[2] = "West";
+            toDir[3] = "South";
 
-        private int GetMod(int width, int height)
-        {
-            return (width - 1) * 2 + (height - 1) * 2;
+            for (int i = 0; i < width; ++i)
+            {
+                pos.Add(new int[] { i, 0 });
+                dir.Add(0);
+            }
+            for (int i = 1; i < height; ++i)
+            {
+                pos.Add(new int[] { width - 1, i });
+                dir.Add(1);
+            }
+            for (int i = width - 2; i >= 0; --i)
+            {
+                pos.Add(new int[] { i, height - 1 });
+                dir.Add(2);
+            }
+            for (int i = height - 2; i > 0; --i)
+            {
+                pos.Add(new int[] { 0, i });
+                dir.Add(3);
+            }
+            dir[0] = 3;
         }
 
         public void Step(int num)
         {
-            num = ModNum(num);
-            while (num > 0)
-            {
-                switch(dir) 
-                {
-                    case Direction.North:
-                        y += num;
-                        num = 0;
-                        if (y > height)
-                        {
-                            num += y - height;
-                            y = height;
-                            dir = Direction.West;
-                        }
-                        break;
-
-                    case Direction.East:
-                        x += num;
-                        num = 0;
-                        if (x > width)
-                        {
-                            num += x - width;
-                            x = width;
-                            dir = Direction.North;
-                        }
-                        break;
-
-
-                    case Direction.South:
-                        y -= num;
-                        num = 0;
-                        if (y < 0)
-                        {
-                            num += Math.Abs(y);
-                            y = 0;
-                            dir = Direction.East;
-                        }
-                        break;
-
-                    case Direction.West:
-                        x -= num;
-                        num = 0;
-                        if (x < 0)
-                        {
-                            num += Math.Abs(x);
-                            x = 0;
-                            dir = Direction.South;
-                        }
-                        break;
-
-                    default:
-                        throw new NotSupportedException($"Direction {dir} is not supported.");
-                }
-            }
+            moved = true;
+            idx = (idx + num) % pos.Count;
         }
 
         public int[] GetPos()
         {
-            return new int[] { x, y };
+            return pos[idx];
         }
 
         public string GetDir()
         {
-            return dir.ToString();
-        }
-
-        public enum Direction
-        {
-            North, East, South, West
-        }
-
-        private int ModNum(int num)
-        {
-            num = num % mod;
-
-            if (num == 0)
+            if (!moved)
             {
-                switch (dir)
-                {
-                    case Direction.North:
-                        dir = Direction.East;
-                        break;
-
-                    case Direction.East:
-                        dir = Direction.South;
-                        break;
-
-                    case Direction.South:
-                        dir = Direction.West;
-                        break;
-
-                    case Direction.West:
-                        dir = Direction.North;
-                        break;
-
-                    default:
-                        throw new NotSupportedException($"Direction {dir} is not supported.");
-                }
+                return "East";
             }
-
-            return num;
+            return toDir[dir[idx]];
         }
     }
 

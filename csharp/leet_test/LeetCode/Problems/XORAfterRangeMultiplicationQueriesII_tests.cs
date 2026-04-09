ae4a74e8edd145d80c236024b21c55805d5b88ab
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-09 08:38:57 -0700

    Add leet solution for XOR after RAnge multiplication queries.

--- csharp/leet_test/LeetCode/Problems/XORAfterRangeMultiplicationQueriesII_tests.cs
+++ csharp/leet_test/LeetCode/Problems/XORAfterRangeMultiplicationQueriesII_tests.cs
@@ -15,23 +15,34 @@ namespace leet_test.LeetCode.Problems
         private static IEnumerable<(int[] nums, int[][] queries, int expected)> cases()
         {
             yield return (
-                    [1, 1, 1],
+                    new int[]
+                    {
+                        2,3,1,5,4
+                    },
                     new int[][]
                     {
-                        [0, 2, 1, 4]
+                        [1,4,2,3],[0,2,1,2]
+                    },
+                    31
+                );
+
+            yield return (
+                    new int[] { 1, 1, 1 },
+                    new int[][]
+                    {
+                        [0,2,1,4]
                     },
                     4
                 );
 
             yield return (
-                    [2, 3, 1, 5, 4],
-                    new int[][]
-                    {
-                        [1,4,2,3],
-                        [0,2,1,2]
-                    },
-                    31
-                );
+                   new int[] { 780 },
+                   new int[][]
+                   {
+                        [0,0,1,13],[0,0,1,17],[0,0,1,9],[0,0,1,18],[0,0,1,16],[0,0,1,6],[0,0,1,4],[0,0,1,11],[0,0,1,7],[0,0,1,18],[0,0,1,8],[0,0,1,15],[0,0,1,12]
+                   },
+                   523618060
+               );
         }
 
         [DataTestMethod]

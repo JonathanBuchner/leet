a1d77fe923569d1f1ba2ba73772f9365b5544477
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-09 09:31:40 -0700

    Add solutiong to kill process.

--- csharp/leet/LeetCode/Problems/KillProcess.cs
+++ csharp/leet/LeetCode/Problems/KillProcess.cs
@@ -13,9 +13,59 @@ namespace leet.LeetCode.Problems.KillProcess
      */
     public class Solution
     {
+        public class Process
+        {
+            public IList<Process> Children { get; set; }
+            public int Pid { get; }
+            public int Ppid { get; }
+
+            public Process(int pid, int ppid)
+            {
+                Pid = pid;
+                Ppid = ppid;
+                Children = new List<Process>();
+            }
+        }
+
         public IList<int> KillProcess(IList<int> pid, IList<int> ppid, int kill)
         {
-            throw new NotImplementedException("Adding tests");
+            var dict = new Dictionary<int, Process>();
+            dict.Add(0, new Process(0, -1)); 
+            
+            // Create a process node for each entry and add to dictionary
+            for (var i = 0; i < pid.Count; i++)
+            {
+                var new_process = new Process(pid[i], ppid[i]);
+                dict.Add(pid[i], new_process);
+            }
+
+            // Connect children to parents; i.e. set Children list
+            for (var i = 0; i < pid.Count; i++)
+            {
+                var child = dict[pid[i]];
+                var parent = dict[ppid[i]];
+                parent.Children.Add(child);
+            }
+
+            // Ready killing off children
+            var result = new List<int>();        
+            var queue = new Queue<Process>();
+
+            var victum = dict[kill];
+            queue.Enqueue(victum);
+
+            while (queue.Count > 0)
+            {
+                var curr = queue.Dequeue();
+                result.Add(curr.Pid);
+                
+                for (var i = 0; i < curr.Children.Count; i++)
+                {
+                    queue.Enqueue(curr.Children[i]);
+                }
+            }
+
+            return result;
         }
     }
 }

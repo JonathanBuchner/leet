f59ad7ebcd861e6177c79360c82c1be1ea78ce9d
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-09 08:14:31 -0700

    Add basic tests for XOR after RAnge multiplication queries.

---
+++ csharp/leet/LeetCode/Problems/XORAfterRangeMultiplicationQueriesII.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.XORAfterRangeMultiplicationQueriesII
+{
+    /**
+     * 3655. XOR After Range Multiplication Queries II
+     * 
+     * https://leetcode.com/problems/xor-after-range-multiplication-queries-ii/
+     */
+    public class Solution
+    {
+        public int XorAfterQueries(int[] nums, int[][] queries)
+        {
+            throw new NotImplementedException("Need to add test.");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/XORAfterRangeMultiplicationQueriesII_tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.XORAfterRangeMultiplicationQueriesII;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class XORAfterRangeMultiplicationQueriesII_tests
+    {
+        private static IEnumerable<(int[] nums, int[][] queries, int expected)> cases()
+        {
+            yield return (
+                    [1, 1, 1],
+                    new int[][]
+                    {
+                        [0, 2, 1, 4]
+                    },
+                    4
+                );
+
+            yield return (
+                    [2, 3, 1, 5, 4],
+                    new int[][]
+                    {
+                        [1,4,2,3],
+                        [0,2,1,2]
+                    },
+                    31
+                );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void XORAfterRangeMultiplicationQueriesII_basic_tests(int[] nums, int[][] queries, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.XorAfterQueries(nums, queries);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

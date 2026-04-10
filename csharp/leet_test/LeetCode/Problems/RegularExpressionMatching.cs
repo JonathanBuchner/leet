6255865a31ff8534063734402737edf0ace83685
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-10 11:30:18 -0700

    Midpoint for regular expression matching.

--- csharp/leet_test/LeetCode/Problems/RegularExpressionMatching.cs
+++ csharp/leet_test/LeetCode/Problems/RegularExpressionMatching.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace leet.LeetCode.Problems.RegularExpressionMatching
@@ -10,7 +11,79 @@ namespace leet.LeetCode.Problems.RegularExpressionMatching
     {
         public bool IsMatch(string s, string p)
         {
-            throw new NotImplementedException("Waiting for tests");
+            return IsMatch(s, p, 0, 0);
+        }
+        public bool IsMatch(string s, string p, int s_i, int p_i)
+        {
+            if (s.Length == 0 || p.Length == 0 || p.StartsWith('*'))
+            {
+                return false;
+            }
+
+            while (true)
+            {
+                if (p_i + 1 < p.Length && p[p_i + 1] == '*')
+                {
+                    var multi = p[p_i];
+                    if (multi == '.')
+                    {
+                        if (p_i + 2 >= p.Length)
+                        {
+                            return true;
+                        }
+
+                        p_i += 2;
+                        var next = p[p_i]; 
+
+                        while (s_i < s.Length)
+                        {
+                            if (IsMatch(s, p, s_i, p_i))
+                            {
+                                return true;
+                            }
+                            s_i++;
+                        }
+
+                        return false; 
+                    }
+                    else
+                    {
+                        while (p[p_i] == multi || p[p_i] == '*')
+                        {
+                            p_i++;
+                            if (p_i >= p.Length)
+                            {
+                                break;
+                            }
+                        }
+
+                        while (s_i < s.Length && s[s_i] == multi)
+                        {
+                            s_i++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (s_i >= s.Length || p_i >= p.Length)
+                    {
+                        break;
+                    }
+
+                    var match = p[p_i];
+
+                    if (match != s[s_i] && match != '.')
+                    {
+                        return false;
+                    }
+
+                    s_i++;
+                    p_i++;
+                    
+                }
+            }
+
+            return p_i == p.Length && s_i == s.Length;
         }
     }
 }
--- csharp/leet_test/LeetCode/Problems/RegularExpressionMatching_tests.cs
+++ csharp/leet_test/LeetCode/Problems/RegularExpressionMatching_tests.cs
@@ -12,13 +12,57 @@ namespace leet_test.LeetCode.Problems
     [TestClass]
     public class RegularExpressionMatching_tests
     {
-        private static IEnumerable<(string s, string p, bool expected)> cases()
+        private static IEnumerable<(string s, string p, bool expected)> cases_basic()
         {
             yield return (
                 "aa",
                 "pp",
                 false
-                );
+            );
+
+            yield return (
+               "aa",
+               "aa",
+               true
+               );
+
+            yield return (
+              "a",
+              "aa",
+              false
+            );
+
+            yield return (
+              "aaa",
+              "a",
+              false
+            );
+
+        }
+
+        private static IEnumerable<(string s, string p, bool expected)> cases_dots()
+        {
+            yield return (
+                "ap",
+                "a.",
+                true
+            );
+
+            yield return (
+               "ap",
+               "..",
+               true
+            );
+
+            yield return (
+               "ap",
+               ".b",
+               false
+            );
+        }
+
+        private static IEnumerable<(string s, string p, bool expected)> cases_star()
+        {
 
             yield return (
                 "aa",
@@ -27,15 +71,116 @@ namespace leet_test.LeetCode.Problems
                 );
 
             yield return (
-                "ab",
-                ".*",
-                true
-                );
+               "aab",
+               "c*a*b",
+               true
+               );
+
+            yield return (
+              "aaa",
+              "a*a",
+              true
+              );
+
+            yield return (
+              "aaaaaaaaa",
+              "a*a*a*a*a*a",
+              true
+              );
+
+            yield return (
+              "a",
+              "a*a*a*a*a*a",
+              true
+              );
+
+            yield return (
+              "c",
+              "a*a*a*ca*a*a",
+              true
+              );
+
+            yield return (
+              "b",
+              "a*b",
+              true
+             );
+
+            yield return (
+             "abcd",
+             "d*",
+             false
+             );
+
+            yield return (
+             "aaa",
+             "*a*c*a",
+             true
+             );
+
+            yield return (
+             "aaa",
+             "ab*a*c*a",
+             true
+             );
+        }
+        private static IEnumerable<(string s, string p, bool expected)> cases_dots_and_star()
+        {
+            yield return (
+               "ab",
+               ".*c",
+               false
+               );
+
+            yield return (
+               "ab",
+               ".*",
+               true
+               );
+
+            yield return (
+               "abcbbbbabbbcabbb",
+               ".*ca.*",
+               true
+               );
         }
 
         [DataTestMethod]
-        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(cases_basic), DynamicDataSourceType.Method)]
         public void RegularExpressionMatching_basic_tests(string s, string p, bool expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.IsMatch(s, p);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases_dots), DynamicDataSourceType.Method)]
+        public void RegularExpressionMatching_dots_tests(string s, string p, bool expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.IsMatch(s, p);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases_star), DynamicDataSourceType.Method)]
+        public void RegularExpressionMatching_star_tests(string s, string p, bool expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.IsMatch(s, p);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases_dots_and_star), DynamicDataSourceType.Method)]
+        public void RegularExpressionMatching_dots_and_star_tests(string s, string p, bool expected)
         {
             var SUT = new Solution();
 

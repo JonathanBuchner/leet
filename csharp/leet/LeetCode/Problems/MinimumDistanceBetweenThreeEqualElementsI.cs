d42e14d577e60274f84d7baee745b9ae7ce1d82b
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-10 09:21:32 -0700

    Add solution to minium distance between three equal elements 1.

--- csharp/leet/LeetCode/Problems/MinimumDistanceBetweenThreeEqualElementsI.cs
+++ csharp/leet/LeetCode/Problems/MinimumDistanceBetweenThreeEqualElementsI.cs
@@ -15,7 +15,43 @@ namespace leet.LeetCode.Problems.MinimumDistanceBetweenThreeEqualElementsI
     {
         public int MinimumDistance(int[] nums)
         {
-            throw new NotImplementedException("Waiting for tests");
+            var dict = GetGoodTuples(nums);
+
+            return GetLowestTuple(dict);
+        }
+
+        private Dictionary<int, List<int>> GetGoodTuples(int[] nums)
+        {
+            var dict = new Dictionary<int, List<int>>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], new List<int>());
+                }
+
+                dict[nums[i]].Add(i);
+            }
+
+            return dict;
+        }
+
+        private int GetLowestTuple(Dictionary<int, List<int>> dict)
+        {
+            var lowest = Int32.MaxValue;
+
+            foreach(var pair in dict)
+            {
+                var list = pair.Value;
+                // Assumes that each list contains indexes in increasing order
+                for (var i = 0; i + 2 < list.Count; i++)
+                {
+                    lowest = Math.Min(lowest, list[i + 2] - list[i]);
+                }
+            }
+
+            return lowest == Int32.MaxValue ? -1 : lowest * 2;
         }
     }
 }

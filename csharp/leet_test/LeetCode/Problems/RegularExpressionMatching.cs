b6e951f07d4284d282ab93d3c4e7ea33a806bf04
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-10 09:29:19 -0700

    Add tests for regular expression mathing.

---
+++ csharp/leet_test/LeetCode/Problems/RegularExpressionMatching.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.RegularExpressionMatching
+{
+    public class Solution
+    {
+        public bool IsMatch(string s, string p)
+        {
+            throw new NotImplementedException("Waiting for tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/RegularExpressionMatching_tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.RegularExpressionMatching;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class RegularExpressionMatching_tests
+    {
+        private static IEnumerable<(string s, string p, bool expected)> cases()
+        {
+            yield return (
+                "aa",
+                "pp",
+                false
+                );
+
+            yield return (
+                "aa",
+                "a*",
+                true
+                );
+
+            yield return (
+                "ab",
+                ".*",
+                true
+                );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void RegularExpressionMatching_basic_tests(string s, string p, bool expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.IsMatch(s, p);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}

911a048b4d9b8033b45de36f00df2b8a8f8ffec6
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-13 09:14:32 -0700

    Add tsts for minimum distqance toype a word using two fingers.

---
+++ csharp/leet/LeetCode/Problems/MinimumDistanceToTypeAWordUsingTwoFingers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumDistanceToTypeAWordUsingTwoFingers
+{
+    /**
+     * Minimum Distance to Type a Word Using Two Fingers
+     * 
+     * https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/description
+     */
+    public class Solution
+    {
+        public int MinimumDistance(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/MinimumDistanceToTypeAWordUsingTwoFingers_tests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinimumDistanceToTypeAWordUsingTwoFingers;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumDistanceToTypeAWordUsingTwoFingers
+    {
+        private static IEnumerable<(string word, int expected)> cases()
+        {
+            yield return (
+                    "CAKE",
+                    3
+                );
+
+            yield return (
+                    "HAPPY",
+                    6
+                );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void MinimumDistanceToTypeAWordUsingTwoFingers_basic_tests(string word, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.MinimumDistance(word);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

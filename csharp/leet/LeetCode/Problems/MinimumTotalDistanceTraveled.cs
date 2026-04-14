d103a9c2bc4e27fe9356ef88bd813015979c90b5
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-14 09:05:49 -0700

    Add tests for minumum total distance traveled.

---
+++ csharp/leet/LeetCode/Problems/MinimumTotalDistanceTraveled.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumTotalDistanceTraveled
+{
+    /**
+     * 2463. Minimum Total Distance Traveled
+     * 
+     * https://leetcode.com/problems/minimum-total-distance-traveled
+     */
+
+    public class Solution
+    {
+        public long MinimumTotalDistance(IList<int> robot, int[][] factory)
+        {
+            throw new NotImplementedException("Waiting for tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/MinimumTotalDistanceTraveled_tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.MinimumTotalDistanceTraveled;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumTotalDistanceTraveled_tests
+    {
+        private static IEnumerable<(IList<int> robot, int[][] factory, int expected)> cases()
+        {
+            yield return (
+                [0, 4, 6],
+                [[2, 2], [6, 2]],
+                4
+                );
+
+            yield return (
+                [1, -1],
+                [[-2, 1], [2, 1]],
+               2
+               );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void MinimumTotalDistanceTraveled_basic_tests(IList<int> robot, int[][] factory, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.MinimumTotalDistance(robot, factory);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

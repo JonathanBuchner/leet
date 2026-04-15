dd8f7f62531d43b7d9a9699a47d101076af0b58b
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-14 18:27:38 -0700

    Add tests for shortest distance to target string in a circular array.

---
+++ csharp/leet/LeetCode/Problems/ShortestDistanceToTargetStringInACircularArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ShortestDistanceToTargetStringInACircularArray
+{
+    /**
+     * Shortest Distance to Target String in a Circular Array
+     * 
+     * https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/
+     */
+    public class Solution
+    {
+        public int ClosestTarget(string[] words, string target, int startIndex)
+        {
+            throw new NotImplementedException("hey");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/ShortestDistanceToTargetStringInACircularArray_tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using leet.LeetCode.Problems.ShortestDistanceToTargetStringInACircularArray;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class ShortestDistanceToTargetStringInACircularArray_tests
+    {
+        private static IEnumerable<(string[] words, string target, int startIndex, int expected)> cases_basic()
+        {
+            yield return (
+                ["hello", "i", "am", "leetcode", "hello"],
+                "hello",
+                1,
+                1
+                );
+
+            yield return (
+                ["a", "b", "leetcode"],
+                "leetcode",
+                0,
+                1
+                );
+
+
+            yield return (
+                 ["i", "eat", "leetcode"],
+                 "ate",
+                 0,
+                 -1
+                 );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases_basic), DynamicDataSourceType.Method)]
+        public void ShortestDistanceToTargetStringInACircularArray_basic_tests(string[] words, string target, int startIndex, int expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.ClosestTarget(words, target, startIndex); 
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+

f69465dc9e87100f66da0fc88df313aae807ae0f
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-16 08:58:21 -0700

    Add tests for closest equal element queries.

---
+++ csharp/leet/LeetCode/Problems/ClosestEqualElementQueries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.ClosestEqualElementQueries
+{
+    public class Solution
+    {
+        public IList<int> SolveQueries(int[] nums, int[] queries)
+        {
+            throw new NotImplementedException("Setting up tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/ClosestEqualElementQueries_tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using leet.LeetCode.Problems.ClosestEqualElementQueries;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class ClosestEqualElementQueries_tests
+    {
+        private static IEnumerable<(int[] nums, int[] queries, int[] expected)> cases()
+        {
+            yield return (
+                [1, 3, 1, 4, 1, 3, 2],
+                [0, 3, 5],
+                [2, -1, 3]
+                );
+
+            yield return (
+               [1, 2, 3, 4],
+               [0, 1, 2, 3],
+               [-1, -1, -1, -1]
+               );
+        }
+
+
+        [DataTestMethod]
+        [DynamicData(nameof(cases), DynamicDataSourceType.Method)]
+        public void ClosestEqualElementQueries_basic_tests(int[] nums, int[] queries, int[] expected)
+        {
+            var SUT = new Solution();
+
+            var actual = SUT.SolveQueries(nums, queries);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

6c087b92e70cfc46c42c1554e3c59ddfc84ca8b8
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-17 09:59:05 -0700

    Add test cases for minimum absouted distance btween mirro pairs.

---
+++ csharp/leet/LeetCode/Problems/MinimumAbsoluteDistanceBetweenMirrorPairs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumAbsoluteDistanceBetweenMirrorPairs
+{
+    public class Solution
+    {
+        public int MinMirrorPairDistance(int[] nums)
+        {
+            throw new NotImplementedException("Waiting for tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/MinimumAbsoluteDistanceBetweenMirrorPairs_tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MinimumAbsoluteDistanceBetweenMirrorPairs;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MinimumAbsoluteDistanceBetweenMirrorPairs_tests
+    {
+        private static IEnumerable<(int[] nums, int expected)> basic_cases()
+        {
+            yield return
+                (
+                    [12, 21, 45, 33, 54],
+                    1
+                );
+
+            yield return
+               (
+                   [120, 21],
+                   1
+               );
+
+            yield return
+               (
+                   [21, 120],
+                   -1
+               );
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(basic_cases), DynamicDataSourceType.Method)]
+        public void MinimumAbsoluteDistanceBetweenMirrorPairs_basic_tests(int[] nums, int expected)
+        {
+            var SUT = new Solution();
+            var actual = SUT.MinMirrorPairDistance(nums);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

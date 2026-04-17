76a74927a856d19e763501d9a7bf8e0146149b78
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-17 10:27:13 -0700

    Add my solution to minimum absolute distance btween mirror pairs.

--- csharp/leet/LeetCode/Problems/MinimumAbsoluteDistanceBetweenMirrorPairs.cs
+++ csharp/leet/LeetCode/Problems/MinimumAbsoluteDistanceBetweenMirrorPairs.cs
@@ -6,11 +6,42 @@ using System.Threading.Tasks;
 
 namespace leet.LeetCode.Problems.MinimumAbsoluteDistanceBetweenMirrorPairs
 {
+    /**
+     * Minimum Absolute Distance Between Mirror Pairs
+     * 
+     * https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/description
+     */
     public class Solution
     {
         public int MinMirrorPairDistance(int[] nums)
         {
-            throw new NotImplementedException("Waiting for tests");
+            var dict = new Dictionary<int, int>();
+            var result = Int32.MaxValue;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    result = Math.Min(result, i - dict[nums[i]]);
+                }
+
+                dict[ReverseNumbers(nums[i])] = i;
+            }
+
+            return result != Int32.MaxValue ? result : -1;
+        }
+
+        private int ReverseNumbers(int num)
+        {
+            var str = num.ToString();
+            var sb = new StringBuilder();
+
+            for (var i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+
+            return Int32.Parse(sb.ToString());
         }
     }
 }

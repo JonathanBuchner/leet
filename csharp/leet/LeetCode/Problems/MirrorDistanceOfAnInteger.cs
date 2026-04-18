240add09c002527e1afe130326cdae35254696c4
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-18 09:30:28 -0700

    Add test cases for Mirro Distance of an integer.

---
+++ csharp/leet/LeetCode/Problems/MirrorDistanceOfAnInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MirrorDistanceOfAnInteger
+{
+    /**
+     * Mirror Distance of an Integer
+     * 
+     * https://leetcode.com/problems/mirror-distance-of-an-integer/description
+     */
+    public class Solution
+    {
+        public int MirrorDistance(int n)
+        {
+            throw new NotImplementedException("Waiting for tests");
+        }
+    }
+}
---
+++ csharp/leet_test/LeetCode/Problems/MirrorDistanceOfAnInteger_tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet.LeetCode.Problems.MirrorDistanceOfAnInteger;
+
+namespace leet_test.LeetCode.Problems
+{
+    [TestClass]
+    public class MirrorDistanceOfAnInteger_tests
+    {
+        private static IEnumerable<(int i, int expected)> basic_cases()
+        {
+            yield return (28, 27);
+            yield return (10, 9);
+            yield return (7, 0);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(basic_cases), DynamicDataSourceType.Method)]
+        public void MirrorDistanceOfAnInteger_basic_tests(int i, int expected)
+        {
+            var SUT = new Solution();
+            var actual = SUT.MirrorDistance(i);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

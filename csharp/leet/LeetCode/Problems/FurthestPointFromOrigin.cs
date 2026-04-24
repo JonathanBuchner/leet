3a9991ec4872daf659a38553881028ab38f7001c
Jonathan Buchner <me@jonathanbuchner.com>
2026-04-24 11:01:54 -0700

    Add solution for furthest point from origin.

--- csharp/leet/LeetCode/Problems/FurthestPointFromOrigin.cs
+++ csharp/leet/LeetCode/Problems/FurthestPointFromOrigin.cs
@@ -10,7 +10,24 @@ namespace leet.LeetCode.Problems.FurthestPointFromOrigin
     {
         public int FurthestDistanceFromOrigin(string moves)
         {
-            throw new NotImplementedException("Waiting for tests.");
+            var left = 0;
+            var right = 0;
+            var wild = 0;
+
+            for (var i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'L')
+                    left++;
+                else if (moves[i] == 'R')
+                    right++;
+                else if (moves[i] == '_')
+                    wild++;
+                else
+                    throw new ArgumentException($"Unrecognized character in string. Character : {moves[i]}");
+            }
+
+
+            return Math.Abs(left - right) + wild;
         }
     }
 }

9b971215e630fd49f9287d139eb935fe073beffd
Jonathan Buchner <me@jonathanbuchner.com>
2026-05-19 09:18:47 -0700

    Add solution to minimum common value.

---
+++ csharp/leet/LeetCode/Problems/MinimumCommonValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet.LeetCode.Problems.MinimumCommonValue
+{
+    public class Solution
+    {
+        public int GetCommon(int[] nums1, int[] nums2)
+        {
+            var i = 0;
+            var k = 0;
+
+            while (i < nums1.Length && k < nums2.Length)
+            {
+                var num1 = nums1[i];
+                var num2 = nums2[k];
+
+                if (num1 == num2)
+                    return num1;
+
+                if (num1 > num2)
+                {
+                    k++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
